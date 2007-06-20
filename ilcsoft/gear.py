5cdec72c1d7ecda9be4ff211aa9ba5510532b6aa
Jan Engels <jan.engels@desy.de>
2007-06-20 12:06:45 +0000

    updated for the new cmake files

--- ilcsoft/gear.py
+++ ilcsoft/gear.py
@@ -13,76 +13,76 @@ from util import *
 
 
 class GEAR(BaseILC):
-	""" Responsible for the GEAR software installation process. """
-	
-	def __init__(self, userInput):
-		BaseILC.__init__(self, userInput, "GEAR", "gear")
+    """ Responsible for the GEAR software installation process. """
+    
+    def __init__(self, userInput):
+        BaseILC.__init__(self, userInput, "GEAR", "gear")
 
-		# supported cmake "build_with" modules
-		self.cmakebuildmodules = []
+        # supported cmake "build_with" modules
+        self.cmakebuildmodules = []
 
-		# java is required for generating header files with ant
-		self.reqmodules_external = [ "Java" ]
+        # java is required for generating header files with ant
+        self.reqmodules_external = [ "Java" ]
 
-		self.reqfiles = [ ["lib/libgear.a", "lib/libgear.so"], ["lib/libgearxml.a", "lib/libgearxml.so"] ]
+        self.reqfiles = [ ["lib/libgear.a", "lib/libgear.so"], ["lib/libgearxml.a", "lib/libgearxml.so"] ]
 
-	def compile(self):
-		""" compile GEAR """
-		
-		os.chdir( self.installPath )
+    def compile(self):
+        """ compile GEAR """
+        
+        os.chdir( self.installPath )
 
-		if( self.rebuild ):
-			if( self.useCMake ):
-				tryunlink( "build/CMakeCache.txt" )
-			else:
-				os.system( "ant clean" )
-				if( os.system( "ant aid.generate 2>&1 | tee -a " + self.logfile ) != 0 ):
-					self.abort( "failed to generate header files!!" )
-		else:
-			if( os.system( "ant aid.generate 2>&1 | tee -a " + self.logfile ) != 0 ):
-				self.abort( "failed to generate header files!!" )
-		
-		# build software
-		if( self.useCMake ):
-			os.chdir( "build" )
-			if( os.system( "cmake " + self.genCMakeCmd() + " .. 2>&1 | tee -a " + self.logfile ) != 0 ):
-				self.abort( "failed to configure!!" )
-			if( os.system( "gmake 2>&1 | tee -a " + self.logfile ) != 0 ):
-				self.abort( "failed to compile!!" )
-			if( os.system( "gmake install 2>&1 | tee -a " + self.logfile ) != 0 ):
-				self.abort( "failed to install!!" )
-		else:
-			if( os.system( "ant cpp 2>&1 | tee -a " + self.logfile ) != 0 ):
-				self.abort( "failed to compile!!" )
-		
-	def buildDocumentation(self):
-		# build documentation
-		if( self.buildDoc ):
-			os.chdir( self.installPath )
-			if(isinPath("doxygen")):
-				print 80*'*' + "\n*** Creating C++ API documentation for " + self.name + " with doxygen...\n" + 80*'*'
-				os.system( "ant cpp.doc 2>&1 | tee -a " + self.logfile )
+        if( self.rebuild ):
+            if( self.useCMake ):
+                tryunlink( "build/CMakeCache.txt" )
+            else:
+                os.system( "ant clean" )
 
-	def init(self):
-	
-		BaseILC.init(self)
-		
-		self.env["GEAR"] = self.installPath
+        # build software
+        if( self.useCMake ):
+            os.chdir( "build" )
+            if( os.system( "cmake " + self.genCMakeCmd() + " .. 2>&1 | tee -a " + self.logfile ) != 0 ):
+                self.abort( "failed to configure!!" )
+            if( os.system( "gmake 2>&1 | tee -a " + self.logfile ) != 0 ):
+                self.abort( "failed to compile!!" )
+            if( os.system( "gmake install 2>&1 | tee -a " + self.logfile ) != 0 ):
+                self.abort( "failed to install!!" )
+        else:
+            if( os.system( "ant aid.generate 2>&1 | tee -a " + self.logfile ) != 0 ):
+                self.abort( "failed to generate header files!!" )
+            if( os.system( "ant cpp 2>&1 | tee -a " + self.logfile ) != 0 ):
+                self.abort( "failed to compile!!" )
+        
+    def buildDocumentation(self):
+        # build documentation
+        if( not self.useCMake and self.buildDoc ):
+            os.chdir( self.installPath )
+            if(isinPath("doxygen")):
+                print 80*'*' + "\n*** Creating C++ API documentation for " + self.name + " with doxygen...\n" + 80*'*'
+                os.system( "ant cpp.doc 2>&1 | tee -a " + self.logfile )
 
-		# PATH
-		self.envpath["PATH"].append( "$GEAR/tools" )
-		self.envpath["PATH"].append( "$GEAR/bin" )
+    def init(self):
+    
+        BaseILC.init(self)
+        
+        self.env["GEAR"] = self.installPath
 
-		# USERINCLUDES + USERLIBS
-		self.envbuild["USERINCLUDES"].append( "-D USE_GEAR -I" + self.installPath + "/src/cpp/include" )
-		self.envbuild["USERLIBS"].append( "-L" + self.installPath + "/lib -lgearxml -lgear" )
+        # PATH
+        self.envpath["PATH"].append( "$GEAR/tools" )
+        self.envpath["PATH"].append( "$GEAR/bin" )
 
-		if( self.mode == "install" ):
-			self.env["GEARVERSION"] = self.version
-			if( self.debug ):
-				self.env["GEARDEBUG"] = "1"
-			
-			# check for doc tools
-			if( self.buildDoc ):
-				if( not isinPath("doxygen")):
-					print "*** WARNING: doxygen was not found!! " + self.name + " documentation will not be built!!! "
+        # USERINCLUDES + USERLIBS
+        self.envbuild["USERINCLUDES"].append( "-D USE_GEAR -I" + self.installPath + "/src/cpp/include" )
+        self.envbuild["USERLIBS"].append( "-L" + self.installPath + "/lib -lgearxml -lgear" )
+
+    def preCheckDeps(self):
+        BaseILC.preCheckDeps(self)
+        
+        if( not self.useCMake and self.mode == "install" ):
+            self.env["GEARVERSION"] = self.version
+            if( self.debug ):
+                self.env["GEARDEBUG"] = "1"
+            
+            # check for doc tools
+            if( self.buildDoc ):
+                if( not isinPath("doxygen")):
+                    print "*** WARNING: doxygen was not found!! " + self.name + " documentation will not be built!!! "
--- ilcsoft/lccd.py
+++ ilcsoft/lccd.py
@@ -13,69 +13,74 @@ from util import *
 
 
 class LCCD(BaseILC):
-	""" Responsible for the LCCD software installation process. """
-	
-	def __init__(self, userInput):
-		BaseILC.__init__(self, userInput, "LCCD", "lccd")
+    """ Responsible for the LCCD software installation process. """
+    
+    def __init__(self, userInput):
+        BaseILC.__init__(self, userInput, "LCCD", "lccd")
 
-		self.reqfiles = [ ["lib/liblccd.a", "lib/liblccd.so"] ]
+        self.reqfiles = [ ["lib/liblccd.a", "lib/liblccd.so"] ]
 
-		# LCIO is required for building LCCD
-		self.reqmodules = [ "LCIO" ]
+        # LCIO is required for building LCCD
+        self.reqmodules = [ "LCIO" ]
 
-		# optional modules
-		self.optmodules = [ "CondDBMySQL", "MySQL" ]
+        # optional modules
+        self.optmodules = [ "CondDBMySQL", "MySQL" ]
 
-		# supported cmake "build_with" modules
-		self.cmakebuildmodules = [ "CondDBMySQL" ]
+        # supported cmake "build_with" modules
+        self.cmakebuildmodules = [ "CondDBMySQL" ]
 
-	def compile(self):
-		""" compile LCCD """
-		
-		os.chdir( self.installPath + "/source" )
+    def compile(self):
+        """ compile LCCD """
+        
+        os.chdir( self.installPath + "/source" )
 
-		if( self.useCMake ):
-			os.chdir( self.installPath + "/build" )
+        if( self.useCMake ):
+            os.chdir( self.installPath + "/build" )
 
-		if( self.rebuild ):
-			if( self.useCMake ):
-				tryunlink( "CMakeCache.txt" )
-			else:
-				os.system( "gmake clean" )
+        if( self.rebuild ):
+            if( self.useCMake ):
+                tryunlink( "CMakeCache.txt" )
+            else:
+                os.system( "gmake clean" )
 
-		# build software
-		if( self.useCMake ):
-			if( os.system( "cmake " + self.genCMakeCmd() + " .. 2>&1 | tee -a " + self.logfile ) != 0 ):
-				self.abort( "failed to configure!!" )
+        # build software
+        if( self.useCMake ):
+            if( os.system( "cmake " + self.genCMakeCmd() + " .. 2>&1 | tee -a " + self.logfile ) != 0 ):
+                self.abort( "failed to configure!!" )
 
-		if( os.system( "gmake 2>&1 | tee -a " + self.logfile ) != 0 ):
-			self.abort( "failed to compile!!" )
-		
-		if( self.useCMake ):
-			if( os.system( "gmake install 2>&1 | tee -a " + self.logfile ) != 0 ):
-				self.abort( "failed to install!!" )
-		
-		# build documentation
-	def buildDocumentation(self):
-		if( self.buildDoc ):
-			os.chdir( self.installPath + "/source" )
-			if(isinPath("doxygen")):
-				print 80*'*' + "\n*** Creating C++ API documentation for " + self.name + " with doxygen...\n" + 80*'*'
-				os.system( "gmake doc 2>&1 | tee -a " + self.logfile )
+        if( os.system( "gmake 2>&1 | tee -a " + self.logfile ) != 0 ):
+            self.abort( "failed to compile!!" )
+        
+        if( self.useCMake ):
+            if( os.system( "gmake install 2>&1 | tee -a " + self.logfile ) != 0 ):
+                self.abort( "failed to install!!" )
+        
+        # build documentation
+    def buildDocumentation(self):
+        if( not self.useCMake and self.buildDoc ):
+            os.chdir( self.installPath + "/source" )
+            if(isinPath("doxygen")):
+                print 80*'*' + "\n*** Creating C++ API documentation for " + self.name + " with doxygen...\n" + 80*'*'
+                os.system( "gmake doc 2>&1 | tee -a " + self.logfile )
 
-	def init(self):
+    def init(self):
 
-		BaseILC.init(self)
-		
-		self.env["LCCD"] = self.installPath
+        BaseILC.init(self)
+        
+        self.env["LCCD"] = self.installPath
+ 
 
-		if( self.mode == "install" ):
+    def preCheckDeps(self):
+        BaseILC.preCheckDeps(self)
 
-			self.env["LCCDVERSION"] = self.version
-			if( self.debug ):
-				self.env["LCCDDEBUG"] = "1"
-			
-			# check for doc tools
-			if( self.buildDoc ):
-				if( not isinPath("doxygen")):
-					print "*** WARNING: doxygen was not found!! " + self.name + " documentation will not be built!!! "
+        if( not self.useCMake and self.mode == "install" ):
+            self.env["LCCDVERSION"] = self.version
+            if( self.debug ):
+                self.env["LCCDDEBUG"] = "1"
+            
+            # check for doc tools
+            if( self.buildDoc ):
+                if( not isinPath("doxygen")):
+                    print "*** WARNING: doxygen was not found!! " + self.name + " documentation will not be built!!! "
+
+           
