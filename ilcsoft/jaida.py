5af54da54648c4038d9181fbfebe96ddb0673d92
Jan Engels <jan.engels@desy.de>
2007-07-09 15:15:32 +0000

    retabed file for whitespaces

--- ilcsoft/jaida.py
+++ ilcsoft/jaida.py
@@ -13,29 +13,29 @@ from util import *
 
 
 class JAIDA(BaseILC):
-	""" Responsible for the JAIDA software installation process. """
-	
-	def __init__(self, userInput):
-		BaseILC.__init__(self, userInput, "JAIDA", "JAIDA")
+    """ Responsible for the JAIDA software installation process. """
+    
+    def __init__(self, userInput):
+        BaseILC.__init__(self, userInput, "JAIDA", "JAIDA")
 
-		self.reqfiles = [ ["bin/aida-setup.sh"], ["lib/aida.jar"] ]
+        self.reqfiles = [ ["bin/aida-setup.sh"], ["lib/aida.jar"] ]
 
-		self.installSupport = False
-		self.hasCMakeSupport = False
+        self.installSupport = False
+        self.hasCMakeSupport = False
 
-		# Java is required for JAIDA
-		self.reqmodules = [ "Java" ]
+        # Java is required for JAIDA
+        self.reqmodules = [ "Java" ]
 
-	def init(self):
+    def init(self):
 
-		BaseILC.init(self)
-		
-		self.env["JAIDA_HOME"] = self.installPath
+        BaseILC.init(self)
+        
+        self.env["JAIDA_HOME"] = self.installPath
         
         # get the jar names in ${JAIDA_HOME}/lib
         jars_fp = glob.glob( self.installPath + "/*.jar" )
-		jars = [ i.split('/')[-1] for i in jars_fp ]
+        jars = [ i.split('/')[-1] for i in jars_fp ]
 
-		for jar in jars:
-			self.envpath["CLASSPATH"].append( "$JAIDA_HOME/lib/" + jar + ".jar" )
+        for jar in jars:
+            self.envpath["CLASSPATH"].append( "$JAIDA_HOME/lib/" + jar + ".jar" )
 
