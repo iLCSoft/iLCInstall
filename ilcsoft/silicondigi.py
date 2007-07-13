a50e4def1726e3f7d1980111921c2809ebc5664c
Jan Engels <jan.engels@desy.de>
2007-07-13 18:57:09 +0000

    added patch for SiliconDigi: creates 3 links for SiDigi, SiDetector & SiClustering so it also works with old makefiles

--- ilcsoft/silicondigi.py
+++ ilcsoft/silicondigi.py
@@ -22,5 +22,8 @@ class SiliconDigi(MarlinPKG):
         # cvs root
         self.download.root="marlinreco"
 
+        # SiliconDigi is made of 3 packages
+        self.sipkgs=["SiDetector","SiDigi","SiClustering"]
+
         # no documentation available
         self.buildDoc = False
