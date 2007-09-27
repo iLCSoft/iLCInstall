8b6aaee2b4c2c52b130b85c6223ff328114a91f7
Jan Engels <jan.engels@desy.de>
2007-09-27 15:21:26 +0000

    bug fix for link mode (G4ENV_INIT)

--- ilcsoft/geant4.py
+++ ilcsoft/geant4.py
@@ -23,17 +23,15 @@ class Geant4(BaseILC):
 
         self.reqfiles = [ ["lib/Linux-g++/libG4run.a", "sharedlib/Linux-g++/libG4run.so"] ]
 
-    def preCheckDeps(self):
-        BaseILC.preCheckDeps(self)
-
-        if( not self.env.has_key( "G4ENV_INIT" )):
-            if( not os.path.exists( self.installPath + "/env.sh" )):
-                self.abort( "you must specify a valid path for a geant4 environment shell script"\
-                        + "e.g. ilcsoft.module(\"Geant4\").env[\"G4ENV_INIT\"]=\"/foo/bar/env.sh\"" )
-            self.env["G4ENV_INIT"]=self.installPath + "/env.sh"
-
     def postCheckDeps(self):
         BaseILC.postCheckDeps(self)
+
+        if( not self.env.has_key( "G4ENV_INIT" )):
+            if( not os.path.exists( self.realPath() + "/env.sh" )):
+                self.abort( "you must specify a valid path for a geant4 environment shell script e.g.:\n"\
+                        + "ilcsoft.module(\"Geant4\").env[\"G4ENV_INIT\"]=\"/foo/bar/env.sh\"" )
+            self.env["G4ENV_INIT"]=self.installPath + "/env.sh"
+
 
         self.env["G4INSTALL"] = self.installPath
         self.envpath["LD_LIBRARY_PATH"].append( "$G4INSTALL/sharedlib/Linux-g++" )
