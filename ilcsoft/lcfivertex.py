d2352a66bd7fb15df709bef599f731220b4a2b00
Jan Engels <jan.engels@desy.de>
2007-11-08 16:49:24 +0000

    added GEAR dependency

--- ilcsoft/lcfivertex.py
+++ ilcsoft/lcfivertex.py
@@ -18,7 +18,7 @@ class LCFIVertex(MarlinPKG):
         MarlinPKG.__init__(self, "LCFIVertex", userInput )
 
         # required modules
-        self.reqmodules = [ "Marlin", "LCIO" ]
+        self.reqmodules = [ "Marlin", "LCIO", "GEAR" ]
 
         # optional modules
         # FIXME not working yet
