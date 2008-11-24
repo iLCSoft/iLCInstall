18408297a82b3e221bc408a90b5339d2c746ff04
Jan Engels <jan.engels@desy.de>
2008-11-24 15:55:42 +0000

    split ant build stuff from cmake stuff (more readable code)

--- ilcsoft/lcio.py
+++ ilcsoft/lcio.py
@@ -52,76 +52,101 @@ class LCIO(BaseILC):
     def compile(self):
         """ compile LCIO """
         
+        # ----- build with cmake -----
         if( self.useCMake ):
+            
             os.chdir( self.installPath+'/build' )
-        
-        if( self.rebuild ):
-            if( self.useCMake ):
+            
+            if self.rebuild:
                 tryunlink( "CMakeCache.txt" )
-            else:
-                os.system( "ant clean" )
-        
-        if( self.useCMake ):
+
             if( os.system( "cmake " + self.genCMakeCmd() + " .. 2>&1 | tee -a " + self.logfile ) != 0 ):
                 self.abort( "failed to configure!!" )
+
             if( os.system( "make ${MAKEOPTS} 2>&1 | tee -a " + self.logfile ) != 0 ):
                 self.abort( "failed to compile!!" )
+
+            # FIXME this should be done in CMakeLists.txt
+            if( not( str(self.envcmake["INSTALL_DOC"]) == "0" or str(self.envcmake["INSTALL_DOC"]) == "OFF" )):
+                if(isinPath("latex")):
+                    print 80*'*' + "\n*** Creating LCIO reference manual (from api doc)...\n" + 80*'*'
+                    os.chdir( self.installPath )
+                    r = os.system( "ant doc.ref 2>&1 | tee -a " + self.logfile )
+                    os.chdir( self.installPath+'/build' )
+                    if( r != 0 ):
+                        print 80*'*' + "*** WARNING: Error creating LCIO reference manual!\n" + 80*'*'
+            
             if( os.system( "make install 2>&1 | tee -a " + self.logfile ) != 0 ):
                 self.abort( "failed to install!!" )
+
+            # execute ctests
+            if( self.makeTests ):
+                
+                if( os.system( "make tests 2>&1 | tee -a " + self.logfile ) != 0 ):
+                    self.abort( "failed to compile lcio tests" )
+                    
+                if( os.system( "make test 2>&1 | tee -a " + self.logfile ) != 0 ):
+                    self.abort( "failed to execute lcio tests" )
+
+        # ----- build with ant -----
         else:
-            # generate headers
+
+            os.chdir( self.installPath )
+            
+            if self.rebuild:
+                os.system( "ant clean" )
+            
             if( os.system( "ant aid.generate 2>&1 | tee -a " + self.logfile ) != 0 ):
                 self.abort( "failed to generate header files!!" )
-            # lcio java stuff
+
             if( self.buildJava ):
                 if( os.system( "ant aid 2>&1 | tee -a " + self.logfile ) != 0 ):
-                    self.abort( "failed to compile lcio java!!" )
+                    self.abort( "failed to execute ant aid!!" )
+
             if( os.system( "ant cpp 2>&1 | tee -a " + self.logfile ) != 0 ):
-                self.abort( "failed to compile!!" )
+                self.abort( "failed to execute ant cpp!!" )
+
             if( self.buildFortran ):
                 if( os.system( "ant f77 2>&1 | tee -a " + self.logfile ) != 0 ):
-                    self.abort( "failed to compile!!" )
+                    self.abort( "failed to execute ant f77!!" )
 
-        # build documentation (only for ant)
-        if( not self.useCMake and self.buildDoc ):
-            # check for doc tools
-            if( not isinPath("javadoc")):
-                print "*** WARNING: javadoc was not found!! Part of the " + self.name + " documentation will not be built!!! "
-            if( not isinPath("doxygen")):
-                print "*** WARNING: doxygen was not found!! Part of the " + self.name + " documentation will not be built!!! "
-            if( not isinPath("latex")):
-                print "*** WARNING: latex was not found!! Part of the " + self.name + " documentation will not be built!!! "
+            if( self.buildDoc ):
+                # check for doc tools
+                if( not isinPath("javadoc")):
+                    print "*** WARNING: javadoc was not found!! Part of the " + \
+                        self.name + " documentation will not be built!!! "
+                if( not isinPath("doxygen")):
+                    print "*** WARNING: doxygen was not found!! Part of the " + \
+                        self.name + " documentation will not be built!!! "
+                if( not isinPath("latex")):
+                    print "*** WARNING: latex was not found!! Part of the " + \
+                        self.name + " documentation will not be built!!! "
 
-            print 80*'*' + "\n*** Building documentation for " + self.name + "...\n" + 80*'*'
-            if( self.buildJava ):
-                if(isinPath("javadoc")):
-                    print 80*'*' + "\n*** Creating JAVA API documentation for " + self.name + " with javadoc...\n" + 80*'*'
-                    os.system( "ant doc 2>&1 | tee -a " + self.logfile )
-            if(isinPath("doxygen")):
-                print 80*'*' + "\n*** Creating C++ API documentation for " + self.name + " with doxygen...\n" + 80*'*'
-                os.system( "ant cpp.doc" )
-            if(isinPath("latex")):
-                print 80*'*' + "\n*** Creating " + self.name + " manual (from tex)...\n" + 80*'*'
-                r = os.system( "ant doc.manual 2>&1 | tee -a " + self.logfile )
-                if( r != 0 ):
-                    print 80*'*' + "*** WARNING: sth. went wrong with creating the " + self.name + " manual\n" + 80*'*'
-                print 80*'*' + "\n*** Creating " + self.name + " reference manual (from api doc)...\n" + 80*'*'
-                r = os.system( "ant doc.ref 2>&1 | tee -a " + self.logfile )
-                if( r != 0 ):
-                    print 80*'*' + "*** WARNING: sth. went wrong with creating the " + self.name + " reference manual\n" + 80*'*'
-        elif( not( str(self.envcmake["INSTALL_DOC"]) == "0" or str(self.envcmake["INSTALL_DOC"]) == "OFF" )):
-            if(isinPath("latex")):
-                os.chdir( self.installPath )
-                print 80*'*' + "\n*** Creating " + self.name + " reference manual (from api doc)...\n" + 80*'*'
-                r = os.system( "ant doc.ref 2>&1 | tee -a " + self.logfile )
-                if( r != 0 ):
-                    print 80*'*' + "*** WARNING: sth. went wrong with creating the " + self.name + " reference manual\n" + 80*'*'
-
-        if( self.useCMake and self.makeTests ):
-            if( os.system( "make tests 2>&1 | tee -a " + self.logfile ) != 0 ):
-                self.abort( "failed to compile lcio tests" )
-            if( os.system( "make test 2>&1 | tee -a " + self.logfile ) != 0 ):
-                self.abort( "failed to execute lcio tests" )
+                # build documentation
+                print 80*'*' + "\n*** Building documentation for " + self.name + "...\n" + 80*'*'
+                
+                if( self.buildJava ):
+                    if(isinPath("javadoc")):
+                        print 80*'*' + "\n*** Creating JAVA API documentation for " + \
+                            self.name + " with javadoc...\n" + 80*'*'
+                        os.system( "ant doc 2>&1 | tee -a " + self.logfile )
+                
+                if(isinPath("doxygen")):
+                    print 80*'*' + "\n*** Creating C++ API documentation for " + \
+                        self.name + " with doxygen...\n" + 80*'*'
+                    os.system( "ant cpp.doc" )
+                
+                if(isinPath("latex")):
+                    print 80*'*' + "\n*** Creating " + self.name + " manual (from tex)...\n" + 80*'*'
+                    r = os.system( "ant doc.manual 2>&1 | tee -a " + self.logfile )
+                    if( r != 0 ):
+                        print 80*'*' + "*** WARNING: sth. went wrong with creating the " + \
+                            self.name + " manual\n" + 80*'*'
+                    print 80*'*' + "\n*** Creating " + self.name + " reference manual (from api doc)...\n" + 80*'*'
+                    r = os.system( "ant doc.ref 2>&1 | tee -a " + self.logfile )
+                    if( r != 0 ):
+                        print 80*'*' + "*** WARNING: sth. went wrong with creating the " + \
+                            self.name + " reference manual\n" + 80*'*'
 
     def preCheckDeps(self):
         BaseILC.preCheckDeps(self)
