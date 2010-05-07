20394e4ccdd8138015b7cada2f0b1338507c55ff
Jan Engels <jan.engels@desy.de>
2010-05-07 10:18:42 +0000

    added fixes for compiling on 64bit

--- ilcsoft/cernlib.py
+++ ilcsoft/cernlib.py
@@ -27,79 +27,111 @@ class CERNLIB(BaseILC):
     def setMode(self, mode):
         BaseILC.setMode(self, mode)
 
-        self.download.url = "http://cernlib.web.cern.ch/cernlib/download/"+self.version+"_source/tar/"
+        urlver=self.version
+
+        if self.version=='2006':
+            urlver='2006b'
+        
+        # don;t remove this line (needed to skip version check in baseilc.py; Version( "2006" ) raises Exception)
+        self.download.url = "http://cernlib.web.cern.ch/cernlib/download/"+urlver+"_source/tar/"
 
     def downloadSources(self):
         
         if( os.path.exists(self.installPath) ):
             return
 
-        # create install base directory
-        trymakedir( os.path.dirname(self.installPath) )
-
+        trymakedir( self.installPath+'/sources' )
         os.chdir( os.path.dirname(self.installPath) )
 
-        # download installation script
-        if( os.system( "wget http://wwwasd.web.cern.ch/wwwasd/cernlib/install/start_cern" ) != 0 ):
-            self.abort( "Problems ocurred downloading sources!!")
-        
-        os.chmod( "start_cern", 0700 )
+        import urllib
 
-        # download index.html
-        if( os.system( "wget " + self.download.url ) != 0 ):
-            self.abort( "Problems ocurred downloading sources!!")
+        if self.version == '2005' :
+            # cernlib fix from Harald Vogt: http://www-zeuthen.desy.de/~hvogt/
+            urllib.urlretrieve( "http://www-zeuthen.desy.de/linear_collider/cernlib/new/cernlib-2005-all-new.tgz", "cernlib-2005-all-new.tgz" )
+            urllib.urlretrieve( "http://www-zeuthen.desy.de/linear_collider/cernlib/new/cernlib.2005.corr.2009.06.13.tgz", "cernlib.2005.corr.2009.06.13.tgz" )
 
-        # extract tar.gz files from index.html and put them into a list
-        self.tgz_files = getoutput( r"grep tar.gz index.html | sed -e 's/.*href=\"\(.*\)\".*/\1/'" ).split('\n')
+            if( os.system( "tar xzf cernlib-2005-all-new.tgz") != 0 ):
+                self.abort("failed to extract cernlib sources")
 
-        # index.html no longer needed
-        os.unlink( "index.html" )
+            # use more recent corrections (64 bit compatible)
+            os.system( "mv cernlib.2005.corr.tgz cernlib.2005.corr.tgz-old && ln -s cernlib.2005.corr.2009.06.13.tgz cernlib.2005.corr.tgz")
 
-        unneeded_files = []
-        
-        # download sources
-        for file in self.tgz_files:
-            if( int(self.version) < 2006 ):
-                if( file.find("src_") == 0 ):
-                    if( os.system( "wget " + self.download.url + file ) != 0 ):
-                        self.abort( "Problems ocurred downloading sources!!" )
-                else:
-                    unneeded_files.append(file)
-            else:
-                if( (file == self.version + "_src.tar.gz") or \
-                        (file == "include.tar.gz" )):
-                    if( os.system( "wget " + self.download.url + file ) != 0 ):
-                        self.abort( "Problems ocurred downloading sources!!" )
-                    # unpack sources
-                    if( os.system( "tar -xzf " + file ) != 0 ):
-                        self.abort( "Problems ocurred unpacking sources!!")
-                else:
-                    unneeded_files.append(file)
+        elif self.version == '2006' :
+            
 
-        # delete unneeded files
-        for file in unneeded_files:
-            self.tgz_files.remove(file)
-    
+            # binary tarballs
+            #if platform.architecture()[0] == '64bit':
+            #    if Version( self.parent.debugInfo['GCC_VERSION'] )[:2] == (4,1) :
+            # ...
+           
+            ## download index.html
+            #if( os.system( "wget " + self.download.url ) != 0 ):
+            #    self.abort( "Problems ocurred downloading sources!!")
+            ## parse index.html for extracting source tarballs
+            #src_tarballs = getoutput( r"grep tar.gz index.html | sed -e 's/.*href=\"\(.*\)\".*/\1/'" ).split('\n')
+            ## index.html no longer needed
+            #os.unlink( "index.html" )
+
+            #index_html=urllib.urlopen( self.download.url ).read()
+
+            #import re
+            #regex=re.compile( 'href="(.*)"' , re.VERBOSE )
+
+            #hrefs=regex.findall( index_html )
+
+            #src_tarballs=[ i.strip() for i in hrefs if i.strip()[-7:] == '.tar.gz' ]
+
+            #for tarball in src_tarballs:
+            #    print 'downloading:', self.download.url + tarball
+            #    urllib.urlretrieve( self.download.url + tarball, tarball )
+            #    print 'extracting:', tarball
+            #    os.system( "tar xzf " + tarball )
+            #    os.system( "mv %s %s/sources" % (tarball, self.installPath) )
+
+            tarball = '2006_src.tar.gz'
+            urllib.urlretrieve( self.download.url + tarball, tarball )
+            if os.system( "tar xzf " + tarball ) != 0:
+                self.abort( 'failed to extract '+ tarball )
+
+
     def compile(self):
-        
+
         os.chdir( os.path.dirname(self.installPath) )
-        
-        #if( self.rebuild ):
-            #os.system( "rm -rf " + self.version )
-        
-        print "Compiling CERNLIB... This takes a LOT of time and displays " \
-                + "no screen output, so just leave it running :)"
-        
-        if( int(self.version) < 2006 ):
-            if( os.system( "./start_cern 2>&1 | tee -a " + self.logfile ) != 0 ):
+
+        # unset MAKEFLAGS
+
+        if self.version == '2005' :
+
+            install_cmd = """
+                type mkdirhier || alias mkdirhier=mkdir
+                ./Install_cernlib
+                test $? -eq 0 || exit 1
+                . cernlib_env
+                
+                # install headers
+                export CERN_INCLUDEDIR=$CERN_ROOT/include
+                mkdir $CERN_INCLUDEDIR
+                cd $CERN_ROOT/build
+                gmake install.include 1>./log/include_install.log 2>&1
+                test $? -eq 0 || exit 1
+
+                # move all build generated stuff into a subdirectory
+                cd $CERN
+                #mkdir $CERN_ROOT/sources
+                mv -f $(ls | grep -v ^${CERN_LEVEL}$) $CERN_ROOT/sources
+            """
+            f = open('install_cernlib.sh', 'w')
+            f.write( install_cmd )
+            f.close()
+
+            if( os.system( "chmod 755 install_cernlib.sh && ./install_cernlib.sh 2>&1 | tee -a " + self.logfile ) != 0 ):
                 self.abort("failed to compile!!")
-        else:
+
+
+        if self.version == '2006' :
             
             os.chdir( self.installPath )
 
-            os.system( "echo \"Compiling CERNLIB... This takes a LOT of time and displays " \
-                + "no screen output, so just leave it running :)\" >> " + self.logfile )
-            
             # create directories
             trymakedir( "bin" )
             trymakedir( "lib" )
@@ -121,9 +153,14 @@ class CERNLIB(BaseILC):
                 self.abort( "failed to compile!!")
             
             os.chdir( "scripts" )
-            print "* Building install.bin..."
-            if( os.system( "make install.bin > ../log/scripts 2>&1" ) != 0 ):
-                self.abort( "failed to compile!!")
+
+            import platform
+
+            # skip install.bin rule on 64bit
+            if platform.architecture()[0] != '64bit': 
+                print "* Building install.bin..."
+                if( os.system( "make install.bin > ../log/scripts 2>&1" ) != 0 ):
+                    self.abort( "failed to compile!!")
 
             os.chdir( self.installPath + "/build" )
             print "* Building libraries..."
@@ -132,23 +169,22 @@ class CERNLIB(BaseILC):
     
     def cleanupInstall(self):
         os.chdir( os.path.dirname(self.installPath) )
-        if( not self.rebuild ):
-            for file in self.tgz_files:
-                tryunlink(file)
-            tryunlink( "start_cern" )
-        os.chdir( self.installPath + "/build" )
+        #if( not self.rebuild ):
+        #    for file in self.tgz_files:
+        #        tryunlink(file)
+        #    tryunlink( "start_cern" )
+        #os.chdir( self.installPath + "/build" )
 
-        # delete object files
-        os.system( "find -type f -name *.o -exec rm -f {} \;" )
+        ## delete object files
+        #os.system( "find -type f -name *.o -exec rm -f {} \;" )
 
     def postCheckDeps(self):
         BaseILC.postCheckDeps(self)
 
         self.envorder=[ "CERN_ROOT" ]
-
         self.env["CERN_ROOT"] = self.installPath
         
-        self.env["CERNLIB_HOME"] = "$CERN_ROOT/lib"
+        #self.env["CERNLIB_HOME"] = "$CERN_ROOT/lib"
         self.env["CVSCOSRC"] = "$CERN_ROOT/src"
         self.env["CERN_LEVEL"] = self.version
         self.env["CERN"] = os.path.dirname(self.installPath)
@@ -158,19 +194,7 @@ class CERNLIB(BaseILC):
 
         if( self.mode =="install" ):
 
-            # imake
-            if( not isinPath( "imake" )):
-                self.abort( "imake not found!!")
-            # makedepend
-            if( not isinPath( "makedepend" )):
-                self.abort( "makedepend not found!!")
-            # cpp
-            if( not isinPath( "cpp" )):
-                self.abort( "cpp not found!!")
-            # grep
-            if( not isinPath( "grep" )):
-                self.abort( "grep not found!!")
-            # sed
-            if( not isinPath( "sed" )):
-                self.abort( "sed not found!!")
-
+            # check if some commands are available
+            for cmd in ['imake', 'makedepend', 'cpp']:
+                if not isinPath( cmd ):
+                    self.abort( cmd+" not found!!" )
