784572c3465e8b9175e42570a3a1cf11b82ad693
Frank Gaede <frank.gaede@desy.de>
2010-10-08 17:49:56 +0000

    ----- pre release versions for v01-10

---
+++ releases/versions_v01-10.py
@@ -0,0 +1,82 @@
+###########################################
+#
+# iLCSoft versions for release v01-10
+#
+# F.Gaede, DESY 07.10.2010
+#
+###########################################
+
+#----- set this to your local install path
+ilc_install_dir = "/afs/desy.de/project/ilcsoft/sw/x86_64_gcc41_sl5/v01-10-pre01"
+
+ROOT_version = "5.26.00b" # -- "5.27.06"   #--- "5.26.00b" 
+
+MySQL_version = "5.0.45"
+
+# Java_version = "1.6.0"
+
+Geant4_version = "9.3"  #--- "9.3.p01"  
+
+CERNLIB_version = "2006" 
+
+#---------------------------------------
+
+
+LCIO_version = "v01-51-01-pre" #--- "v01-51" 
+
+GEAR_version = "v00-15-pre"    #-- "v00-14-02" 
+
+KalTest_version = "v01-00-pre"       #-- "v00-14-02" 
+
+KalDet_version = "v01-00-pre"       #-- "v00-14-02" 
+
+CondDBMySQL_version = "CondDBMySQL_ILC-0-9-1-pre"   #--- "CondDBMySQL_ILC-0-8-1" # ??? 
+
+LCCD_version = "v01-01-pre"  #--- "v01-00" 
+
+RAIDA_version = "v01-05-pre" 
+
+CED_version = "v01-01-pre"   #--- "v01-00" 
+
+MarlinUtil_version = "v01-01-pre"  #---- "v01-00" 
+
+Marlin_version = "v01-13-pre"  #---- "v00-12" 
+
+Mokka_version = "HEAD"     # --- "mokka-07-05 or 06 ?" 
+
+MarlinReco_version = "HEAD"  #----- "v00-18-02"  
+
+PandoraPFA_version = "v03-02-02-pre" 
+
+PandoraPFANew_version = "v00-03"  #--- "v00-02" 
+MarlinPandora_version = "v00-02"  #--- "v00-01" 
+
+LCFIVertex_version = "v00-04-pre" 
+
+CEDViewer_version = "v01-01-pre"  
+
+Overlay_version = "v00-07-03"
+
+
+StandardConfig_version = "v02-01" 
+MokkaDBConfig_version = "v02-01"
+LCFI_MokkaBasedNets_version = "v00-01" 
+
+
+Eutelescope_version = "v00-04-04-pre"     
+
+MarlinTPC_version = "v00-05-02"  #---- ????
+
+SiliconDigi_version = "v00-04-02" 
+
+Druid_version = "1.8-pre" 
+
+CLHEP_version = "2.0.4.2" 
+
+GSL_version = "1.8" 
+
+QT_version = "4.2.2" 
+
+CMake_version = "2.6.2" 
+
+CMakeModules_version = "v01-10-pre"  #--- "v01-10" 
