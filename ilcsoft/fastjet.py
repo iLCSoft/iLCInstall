0ec43c463f35d7274f2277f8bed196b0ec558206
Jan Engels <jan.engels@desy.de>
2010-12-02 15:23:54 +0000

    added module for fastjet (A.Sailer)

---
+++ ilcsoft/fastjet.py
@@ -0,0 +1,73 @@
+##################################################
+#
+# FastJet module
+#
+# Author: Andre Sailer, CERN
+# based on GSL module by J. Engels, Desy
+# Date: Jul, 2010
+#
+##################################################
+                                                                                                                                                            
+# custom imports
+from baseilc import BaseILC
+from util import *
+
+
+class FastJet(BaseILC):
+    """ Responsible for the FastJet installation process. """
+    
+    def __init__(self, userInput):
+        BaseILC.__init__(self, userInput, "FastJet", "FastJet")
+
+        # no cmake build support
+        self.hasCMakeBuildSupport = False
+        
+        self.download.supportHEAD = False
+        self.download.supportedTypes = ["wget"]
+
+        self.reqfiles = [[
+                "lib/libfastjet.so",
+        ]]
+    
+    def setMode(self, mode):
+        BaseILC.setMode(self, mode)       
+        self.download.url = "http://www.lpthe.jussieu.fr/~salam/fastjet/repo/fastjet-" + self.version + ".tar.gz"
+        
+    def downloadSources(self):
+        BaseILC.downloadSources(self)
+
+        # move sources to a subdirectory
+        os.renames( self.version, self.name )
+        os.renames( self.name, self.version + "/" + self.name )
+
+        # create build directory
+        trymakedir( self.installPath + "/build" )
+
+    def compile(self):
+        """ compile FastJet """
+
+        os.chdir( self.installPath + "/build" )
+
+        if( self.rebuild ):
+            os.system( "make distclean" )
+
+        if( os.system( "../" + self.name + "/configure --prefix=" + self.installPath + " --enable-shared 2>&1 | tee -a " + self.logfile ) != 0 ):
+            self.abort( "failed to configure!!" )
+
+        if( os.system( "make ${MAKEOPTS} 2>&1 | tee -a " + self.logfile ) != 0 ):
+            self.abort( "failed to compile!!" )
+
+        if( os.system( "make install 2>&1 | tee -a " + self.logfile ) != 0 ):
+            self.abort( "failed to install!!" )
+
+    def cleanupInstall(self):
+        BaseILC.cleanupInstall(self)
+        os.chdir( self.installPath + "/build" )
+        os.system( "make clean" )
+
+    def postCheckDeps(self):
+        BaseILC.postCheckDeps(self)
+
+        self.env["FastJet_HOME"] = self.installPath
+        self.envpath["PATH"].append( "$FastJet_HOME/bin" )
+        self.envpath["LD_LIBRARY_PATH"].append( "$FastJet_HOME/lib" )
