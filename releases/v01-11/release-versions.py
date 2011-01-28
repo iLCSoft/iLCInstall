c23d8991cd3800dc89e7c031389a3b99accbf043
Jan Engels <jan.engels@desy.de>
2011-01-28 15:55:16 +0000

    updated versions

--- releases/v01-11/release-versions.py
+++ releases/v01-11/release-versions.py
@@ -13,7 +13,8 @@ ilcsoft_release='v01-11-pre'
 
 
 # --------- install dir ------------------------------------------------------
-ilcsoft_install_prefix = "/scratch/$USER/ilcsoft/"
+#ilcsoft_install_prefix = "/scratch/$USER/ilcsoft/"
+ilcsoft_install_prefix = ilcsoft_afs_path[ arch ]
 #ilcsoft_install_dir = os.path.join( ilcsoft_install_prefix, ilcsoft_release )
 # ----------------------------------------------------------------------------
 
@@ -53,24 +54,24 @@ CMake_version = "2.8.3"
 
 # -------------------------------------------
 
-LCIO_version = "v01-51-02"
+LCIO_version = "v01-51-02" # update?
 
-GEAR_version = "v00-16"
+GEAR_version = "v00-17-pre"
 
-CED_version = "v01-01-01"
+CED_version = "v01-01-01" # update?
 
-CondDBMySQL_version = "CondDBMySQL_ILC-0-9-2"
+CondDBMySQL_version = "CondDBMySQL_ILC-0-9-2" # update?
 
 #CMakeModules_version = "v01-10-01" # DEPRECATED
-ILCUTIL_version = "v00-01-pre" # FIXME
+ILCUTIL_version = "v00-01-pre"
 
-dcap_version = "1.9.5-5"
+dcap_version = "1.9.5-5" # remove?
 
 FastJet_version = "2.4.2"
-FastJetClustering_version = "v00-01"
+FastJetClustering_version = "v00-01" # TODO
 
 
-StandardConfig_version = "v02-02"
+StandardConfig_version = "v02-02" # update?
 MokkaDBConfig_version = "v02-02"
 LCFI_MokkaBasedNets_version = "v00-01" 
 
@@ -78,40 +79,40 @@ LCFI_MokkaBasedNets_version = "v00-01"
 
 # -------------------------------------------
 
-KalTest_version = "v01-00"
+KalTest_version = "v01-00" # TODO
 
-KalDet_version = "v01-00"
+KalDet_version = "v01-00" # TODO
 
-LCCD_version = "v01-01"
+LCCD_version = "v01-02-pre"
 
-RAIDA_version = "v01-05" 
+RAIDA_version = "v01-06-pre" 
 
-MarlinUtil_version = "v01-01"
+MarlinUtil_version = "v01-02-pre"
 
-Marlin_version = "v00-13-01"
+Marlin_version = "v00-13-01" # TODO
 
-Mokka_version = "mokka-07-06"
+Mokka_version = "mokka-07-06" # update?
 
-MarlinReco_version = "v00-19"
+MarlinReco_version = "v00-19" # TODO
 
-PandoraPFA_version = "v03-02-02" 
+PandoraPFA_version = "v03-02-02" # update/remove??
 
-PandoraPFANew_version = "v00-04"
-MarlinPandora_version = "v00-03"
+PandoraPFANew_version = "v00-04" # TODO
+MarlinPandora_version = "v00-03" # TODO
 
-LCFIVertex_version = "v00-04" 
+LCFIVertex_version = "v00-04" # TODO
 
-CEDViewer_version = "v01-01-01"
+CEDViewer_version = "v01-01-01" # TODO
 
-Overlay_version = "v00-08"
+Overlay_version = "v00-08" # TODO
 
-Eutelescope_version = "v00-04-04"     
+Eutelescope_version = "v00-04-04" # TODO    
 
-MarlinTPC_version = "v00-08-03"
+MarlinTPC_version = "v00-08-03" # TODO
 
-SiliconDigi_version = "v00-04-02" 
+SiliconDigi_version = "v00-04-02" # update/remove??
 
 Druid_version = "1.8" 
 
-Garlic_version = "v2.0.3"
+Garlic_version = "v2.0.3" # TODO
 
