12331075cb29271c88daeba0416d046053b39765
Jan Engels <jan.engels@desy.de>
2011-08-15 08:04:48 +0000

    added cmake find support for MarlinTPC

--- ilcsoft/marlinpkg.py
+++ ilcsoft/marlinpkg.py
@@ -31,7 +31,7 @@ class MarlinPKG(BaseILC):
         self.reqfiles = [ [ str("lib/lib" + name + ".a"), str("lib/lib" + name + ".so"), str("lib/lib" + name + ".dylib") ] ]
         self.reqmodules=[ 'LCIO', 'Marlin' ]
 
-        if( self.name != "MarlinReco" ):
+        if( self.name != "MarlinReco" and self.name != "MarlinTPC" ):
             self.hasCMakeFindSupport = False
     
     def compile(self):
