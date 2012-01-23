bde7e40b157ddbe5c1833138f371903281cba76c
Jan Engels <jan.engels@desy.de>
2012-01-23 17:34:09 +0000

    updated version of StandardConfig

--- releases/v01-13-02/release-versions.py
+++ releases/v01-13-02/release-versions.py
@@ -92,7 +92,7 @@ FastJetClustering_version = "v00-02"
 MarlinFastJet_version = "v00-01"
 
 
-StandardConfig_version = "v03-51"
+StandardConfig_version = "v03-51-01"
 MokkaDBConfig_version = "v03-06"
 LCFI_MokkaBasedNets_version = "v00-01" 
 
