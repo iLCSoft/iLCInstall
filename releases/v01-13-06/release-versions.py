fb58e0716bdef21d80c79a2d4f386395fb9cfc7a
Jan Engels <jan.engels@desy.de>
2012-05-02 10:07:55 +0000

    added BBQ version

--- releases/v01-13-06/release-versions.py
+++ releases/v01-13-06/release-versions.py
@@ -1,6 +1,6 @@
 ###########################################
 #
-# iLCSoft versions for release v01-13-05
+# iLCSoft versions for release v01-13-06
 #
 # F.Gaede, DESY 09.12.2011
 #
@@ -8,13 +8,13 @@
 
 
 # --------- ilcsoft release version ------------------------------------------
-ilcsoft_release='v01-13-05'
+ilcsoft_release='v01-13-06-pre00'
 # ----------------------------------------------------------------------------
 
 
 # --------- install dir ------------------------------------------------------
 ilcsoft_install_prefix = "/scratch/$USER/ilcsoft/"
-#ilcsoft_install_prefix = ilcsoft_afs_path[ arch ]
+ilcsoft_install_prefix = ilcsoft_afs_path[ arch ]
 #ilcsoft_install_dir = os.path.join( ilcsoft_install_prefix, ilcsoft_release )
 # ----------------------------------------------------------------------------
 
@@ -62,11 +62,11 @@ Geant4_version = "9.5.p01"
 
 ROOT_version = "5.28.00f"
 
-CLHEP_version = "2.1.1.0"  # "2.1.0.1"
+CLHEP_version = "2.1.1.0"
 
 GSL_version = "1.14"
 
-QT_version = "4.7.4" # "4.2.2"
+QT_version = "4.7.4"
 
 CMake_version = "2.8.5"
 
@@ -74,11 +74,11 @@ CMake_version = "2.8.5"
 
 # -------------------------------------------
 
-LCIO_version = "v02-01-01" # "v02-01"
+LCIO_version = "v02-01-01"
 
-GEAR_version = "v01-02" # "v01-01"
+GEAR_version = "v01-02"
 
-CED_version = "v01-05" # "v01-05"
+CED_version = "v01-05"
 
 CondDBMySQL_version = "CondDBMySQL_ILC-0-9-5"
 
@@ -91,49 +91,51 @@ MarlinFastJet_version = "v00-01"
 
 # -------------------------------------------
 
-KalTest_version = "v01-05" # "v01-04"
+KalTest_version = "v01-05"
 
-KalDet_version = "v01-07" # "v01-06"
+KalDet_version = "v01-07"
 
 LCCD_version = "v01-02"
 
 RAIDA_version = "v01-06-02"
 
-MarlinUtil_version = "v01-05-01" # "v01-05"
+MarlinUtil_version = "v01-05-01"
 
-Marlin_version = "v01-02-01" # "v01-02"
+Marlin_version = "v01-02-01"
 
-Mokka_version = "mokka-07-07-p07" # "mokka-07-07-p06"
+Mokka_version = "mokka-07-07-p07"
 
-MarlinReco_version = "v01-01-01" # "v01-01"
+MarlinReco_version = "v01-01-01"
 
-MarlinTrk_version = "v01-05" # "v01-04"
+MarlinTrk_version = "v01-05"
 
-MarlinTrkProcessors_version = "v01-04" # "v01-03"
+MarlinTrkProcessors_version = "v01-04"
 
-Clupatra_version = "v00-05-01" # "v00-04-01"
+Clupatra_version = "v00-05-01"
 
-LCFIPlus_version = "v00-03" # "v00-03"
+LCFIPlus_version = "v00-03" 
 
-ForwardTracking_version = "v01-02" # "v01-01-01"
+ForwardTracking_version = "v01-02"
 
-MarlinKinfit_version = "v00-01-01" # "v00-01"
+MarlinKinfit_version = "v00-01-01"
 
-PandoraPFANew_version = "v00-08" # "v00-08"
-MarlinPandora_version = "v00-07" # "v00-07"
-PandoraAnalysis_version = "v00-03" # "v00-03"
+PandoraPFANew_version = "v00-08"
+MarlinPandora_version = "v00-07"
+PandoraAnalysis_version = "v00-03"
 
 
 LCFIVertex_version = "v00-06-01"
 
-CEDViewer_version = "v01-05" # "v01-04-01"
+CEDViewer_version = "v01-05"
 
 Overlay_version = "v00-11-01"
 
 #Eutelescope_version = "v00-06-03"
 
-PathFinder_version =  "v00-01-01" # "v00-01-01"
-MarlinTPC_version =  "v00-09-01" # "v00-09-01"
+PathFinder_version =  "v00-01-01"
+MarlinTPC_version =  "v00-09-01"
+
+BBQ_version =  "HEAD"
 
 Druid_version = "1.8" 
 
