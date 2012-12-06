f6445967dc47ede52f61f5c4314a25a3479c8c55
Jan Engels <jan.engels@desy.de>
2012-12-06 11:04:10 +0000

    added check of version in CMakeLists

--- scripts/tagpackage.sh
+++ scripts/tagpackage.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-url=$(svn info $1| grep URL | cut -d' ' -f2)
+package_dir=${1:-.}
+
+url=$(svn info $package_dir| grep URL | cut -d' ' -f2)
 
 case $url in
     https://svnsrv.desy.de/public/* ) url=$(echo $url | sed 's|/public/|/desy/|') ; echo "changed url from /public/ to /desy/" ;;
@@ -33,8 +35,12 @@ echo dest url: $base_url/tags/$tag
 fi
 
 echo
+echo CMakeLists.txt:
+grep _VERSION_ $package_dir/CMakeLists.txt
+
+echo
 echo list lib dir:
-ls ${1:-.}/lib
+ls $package_dir/lib
 
 echo
 echo list branches:
