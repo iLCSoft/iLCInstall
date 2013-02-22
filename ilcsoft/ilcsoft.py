ade9941fb9e6fcd6ee8dc5d6cdad30edb47ec15d
Christoph Rosemann <christoph.rosemann@desy.de>
2013-02-22 13:25:03 +0000

    Put an additional environment variable during installation into init script for SL6

--- ilcsoft/ilcsoft.py
+++ ilcsoft/ilcsoft.py
@@ -62,12 +62,14 @@ class ILCSoft:
         self.debugInfo['UNAME']=getoutput( "uname -a" ).strip()
         self.debugInfo['LSB_RELEASE']=getoutput( "lsb_release -a 2>/dev/null" ).strip()
         self.debugInfo['GCC_VERSION']=getoutput( "gcc --version | head -n1" ).strip()
-
-        print
+        release_string = getoutput( "lsb_release -d 2>/dev/null").strip()
+        self.release_number = release_string[re.search('\d', release_string).start()]
+        
         for k,v in self.debugInfo.iteritems():
             print "+", k, '\t', str(v).replace("\n","\n\t\t")
 
         print
+
 
     
     def use(self, module):
@@ -375,7 +377,10 @@ class ILCSoft:
         if self.os.type == "Darwin":
             f.write( os.linesep + '# --- set DYLD_LIBRARY_PATH to LD_LIBRARY_PATH for MAC compatibility ---' + os.linesep )
             f.write( 'export DYLD_LIBRARY_PATH=$LD_LIBRARY_PATH:$DYLD_LIBRARY_PATH' + os.linesep + os.linesep )
-        f.close()
+        if ( self.os.ver.find("cientific") and self.release_number == '6'):
+            f.write( os.linesep + '# --- MOKKA needs a special environment variable under SL6  ---' + os.linesep )
+            f.write( 'export LIBGL_ALWAYS_INDIRECT=1' + os.linesep  )
+
 
         print "\n" + 30*'*' + " Creating symlinks " + 30*'*' + "\n"
         for mod in self.modules:
