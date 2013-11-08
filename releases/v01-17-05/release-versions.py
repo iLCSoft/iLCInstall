149934284f3c2bdcbd375f25c31d0ccd89d25d61
Frank Gaede <frank.gaede@desy.de>
2013-11-08 15:03:46 +0000

    --- started to prepare v01-17-05     (increased CEDViewer version to v01-07-01)

--- releases/v01-17-05/release-versions.py
+++ releases/v01-17-05/release-versions.py
@@ -8,7 +8,7 @@
 
 
 # --------- ilcsoft release version ------------------------------------------
-ilcsoft_release='v01-17-04'
+ilcsoft_release='v01-17-05'
 # ----------------------------------------------------------------------------
 
 
@@ -133,7 +133,7 @@ PandoraPFANew_version = "v00-12" #"v00-09"
 MarlinPandora_version = "v00-11" #"v00-09-02"
 PandoraAnalysis_version = "v00-05" #"v00-04"
 
-CEDViewer_version = "v01-07"
+CEDViewer_version = "v01-07-01"
 
 Overlay_version = "v00-13"
 
