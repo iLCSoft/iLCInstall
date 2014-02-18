88335cddff251b31a9235ed43541e00bb259f3bc
Jeremy McCormick <jeremym@slac.stanford.edu>
2014-02-18 23:47:23 +0000

    Cleanup and minor version changes to dev config.  (Most people should NOT use this config.)

--- examples/slic/dev-versions.py
+++ examples/slic/dev-versions.py
@@ -8,56 +8,22 @@
 
 
 # --------- ilcsoft release version ------------------------------------------
-#ilcsoft_release='v00-00'
 ilcsoft_release='dev'
 # ----------------------------------------------------------------------------
 
 
 # --------- install dir ------------------------------------------------------
-ilcsoft_install_prefix = "/scratch/$USER/slic/"
-
-#ilcsoft_install_prefix = ilcsoft_afs_path[ arch ]
-#ilcsoft_install_prefix = "/scratch/gaede/ilcsoft/"
-#ilcsoft_install_prefix = "/scratch/rosem/ilcsoft/"
-
-#ilcsoft_install_dir = os.path.join( ilcsoft_install_prefix, ilcsoft_release )
-# ----------------------------------------------------------------------------
-
-
-# --------- ilcsoft home -----------------------------------------------------
-# python variable for referring the ILC Home directory
-# used to link or use already installed packages (SL4 or SL5)
-# no need to set this variable if using SL4 or SL5 with access to /afs/desy.de/
-#ilcPath = '/afs/desy.de/project/ilcsoft/sw/x86_64_gcc44_sl6'
-#ilcPath = '/afs/desy.de/project/ilcsoft/sw/x86_64_gcc41_sl5'
-#ilcPath = ilcsoft_afs_path[ arch ]
-#fg? ilcPath = ilcsoft_install_prefix
-# ----------------------------------------------------------------------------
+#ilcsoft_install_prefix = "/scratch/$USER/slic/"
+ilcsoft_install_prefix = "/u1/projects/slic"
 
 # ======================= PACKAGE VERSIONS ===================================
 
-# xerces-c (needed by geant4 for building gdml support - required by mokka)
-#XERCESC_ROOT_DIR = ilcPath + "/xercesc/2.7.0"
-
-# ----- java ---------------------------------------------------------
-#Java_version = "1.6.0"
-#Java_path = ilcPath + "/java/" + Java_version # comment out to try auto-detect
-#Java_path = "/usr/"
-
 XercesC_version = "3.1.1" 
-
-LCIO_version = "v02-03-03"
-
+LCIO_version = "HEAD"
 ILCUTIL_version = "v01-00"
-
-Geant4_version = "9.6.p01"
-
-CMake_version = "2.8.5"
-
+Geant4_version = "10.00"
+CMake_version = "2.8.6"
 HepPDT_version = "3.04.01"
-
 GDML_version = "HEAD"
-
-LCDD_version = "HEAD"
-
-SLIC_version = "HEAD"
+LCDD_version="v00-00-01_LCDD-104-dev"
+SLIC_version = "v00-00-01_SLIC-218-dev"  
