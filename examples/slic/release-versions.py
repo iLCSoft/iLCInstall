068fed7b7399aa405b2230d0f4b2d1019ded0ed1
Jeremy McCormick <jeremym@slac.stanford.edu>
2014-02-18 23:46:06 +0000

    Switch to LCDD 3.3.1 and cleanup.

--- examples/slic/release-versions.py
+++ examples/slic/release-versions.py
@@ -1,63 +1,26 @@
 ###########################################
 #
-# iLCSoft versions for release v01-16
+# SLIC versions for v03-01-04 release
 #
-# F.Gaede, DESY 09.12.2011
+# Jeremy McCormick 
 #
 ###########################################
 
-
 # --------- ilcsoft release version ------------------------------------------
-ilcsoft_release='release_v03-01-04'
+ilcsoft_release='v03-01-04'
 # ----------------------------------------------------------------------------
-
 
 # --------- install dir ------------------------------------------------------
 #ilcsoft_install_prefix = "/scratch/$USER/slic/"
 ilcsoft_install_prefix = "/u1/projects/slic/"
 
-#ilcsoft_install_prefix = ilcsoft_afs_path[ arch ]
-#ilcsoft_install_prefix = "/scratch/gaede/ilcsoft/"
-#ilcsoft_install_prefix = "/scratch/rosem/ilcsoft/"
-
-#ilcsoft_install_dir = os.path.join( ilcsoft_install_prefix, ilcsoft_release )
-# ----------------------------------------------------------------------------
-
-
-# --------- ilcsoft home -----------------------------------------------------
-# python variable for referring the ILC Home directory
-# used to link or use already installed packages (SL4 or SL5)
-# no need to set this variable if using SL4 or SL5 with access to /afs/desy.de/
-#ilcPath = '/afs/desy.de/project/ilcsoft/sw/x86_64_gcc44_sl6'
-#ilcPath = '/afs/desy.de/project/ilcsoft/sw/x86_64_gcc41_sl5'
-#ilcPath = ilcsoft_afs_path[ arch ]
-#fg? ilcPath = ilcsoft_install_prefix
-# ----------------------------------------------------------------------------
-
 # ======================= PACKAGE VERSIONS ===================================
-
-# xerces-c (needed by geant4 for building gdml support - required by mokka)
-#XERCESC_ROOT_DIR = ilcPath + "/xercesc/2.7.0"
-
-# ----- java ---------------------------------------------------------
-Java_version = "1.6.0"
-#Java_path = ilcPath + "/java/" + Java_version # comment out to try auto-detect
-Java_path = "/usr/"
-
 XercesC_version = "3.1.1" 
-
-LCIO_version = "v02-03-03"
-
+LCIO_version = "v02-04-03"
 ILCUTIL_version = "v01-00"
-
 Geant4_version = "9.6.p01"
-
 CMake_version = "2.8.5"
-
 HepPDT_version = "3.04.01"
-
 GDML_version = "v03-00-02"
-
-LCDD_version = "v03-03-00"
-
+LCDD_version = "v03-03-01"
 SLIC_version = "v03-01-04"
