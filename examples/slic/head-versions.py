81ecf38ef82b4963d431483ca87261562ca76820
Jeremy McCormick <jeremym@slac.stanford.edu>
2014-02-20 19:08:33 +0000

    Add HEAD config.  (Most people should not use this\!)

---
+++ examples/slic/head-versions.py
@@ -0,0 +1,31 @@
+###########################################
+#
+# iLCSoft versions for release v01-16
+#
+# F.Gaede, DESY 09.12.2011
+#
+###########################################
+
+
+# --------- ilcsoft release version ------------------------------------------
+#ilcsoft_release='v00-00'
+ilcsoft_release='HEAD'
+# ----------------------------------------------------------------------------
+
+
+# --------- install dir ------------------------------------------------------
+#ilcsoft_install_prefix = "/scratch/$USER/slic/"
+ilcsoft_install_prefix = "/u1/projects/slic"
+
+# ======================= PACKAGE VERSIONS ===================================
+
+XercesC_version = "3.1.1" 
+LCIO_version = "HEAD"
+ILCUTIL_version = "v01-00"
+Geant4_version = "9.6.p01"
+#Geant4_version = "10.00"
+CMake_version = "2.8.12.2"
+HepPDT_version = "3.04.01"
+GDML_version = "HEAD"
+LCDD_version = "HEAD"
+SLIC_version = "HEAD"
