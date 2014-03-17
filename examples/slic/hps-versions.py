912dbbdd9d24add070b2d8ca9d0f075670cba5d9
Jeremy McCormick <jeremym@slac.stanford.edu>
2014-03-17 19:02:21 +0000

    Use new LCDD version for HPS config.

--- examples/slic/hps-versions.py
+++ examples/slic/hps-versions.py
@@ -7,7 +7,7 @@
 ###########################################
 
 # --------- ilcsoft release version ------------------------------------------
-ilcsoft_release='hps_v01-00-01'
+ilcsoft_release='hps_v01-00-02'
 # ----------------------------------------------------------------------------
 
 # --------- install dir ------------------------------------------------------
@@ -15,12 +15,13 @@ ilcsoft_release='hps_v01-00-01'
 ilcsoft_install_prefix = "/u1/projects/slic/"
 
 # ======================= PACKAGE VERSIONS ===================================
+
+CMake_version = "2.8.12.2"
+ILCUTIL_version = "v01-00"
 XercesC_version = "3.1.1" 
 LCIO_version = "v02-04-03"
-ILCUTIL_version = "v01-00"
 Geant4_version = "9.6.p01"
-CMake_version = "2.8.12.2"
 HepPDT_version = "3.04.01"
 GDML_version = "v03-00-02"
-LCDD_version = "v03-03-02"
+LCDD_version = "v03-03-04"
 SLIC_version = "v03-01-04"
