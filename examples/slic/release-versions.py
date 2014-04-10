d70cf093fd6bad3e082103b3fd7e41a097b088e3
Jeremy McCormick <jeremym@slac.stanford.edu>
2014-04-10 08:02:08 +0000

    This release builds fine.  The one from earlier today was broken.

--- examples/slic/release-versions.py
+++ examples/slic/release-versions.py
@@ -7,12 +7,12 @@
 ###########################################
 
 # --------- ilcsoft release version ------------------------------------------
-ilcsoft_release='v03-01-04'
+ilcsoft_release='v03-01-06'
 # ----------------------------------------------------------------------------
 
 # --------- install dir ------------------------------------------------------
-#ilcsoft_install_prefix = "/scratch/$USER/slic/"
-ilcsoft_install_prefix = "/u1/projects/slic/"
+ilcsoft_install_prefix = "/scratch/$USER/slic/"
+#ilcsoft_install_prefix = "/u1/projects/slic/"
 
 # ======================= PACKAGE VERSIONS ===================================
 XercesC_version = "3.1.1" 
@@ -22,5 +22,5 @@ Geant4_version = "9.6.p01"
 CMake_version = "2.8.5"
 HepPDT_version = "3.04.01"
 GDML_version = "v03-00-02"
-LCDD_version = "v03-03-03"
-SLIC_version = "v03-02-02"
+LCDD_version = "v03-03-04"
+SLIC_version = "v03-01-06"
