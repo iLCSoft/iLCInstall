0271404fd3ad64393d01e4ac486c219cb2ee8ed8
Jeremy McCormick <jeremym@slac.stanford.edu>
2014-04-21 20:04:17 +0000

    Change to current development branches and update other package versions.

--- examples/slic/dev-versions.py
+++ examples/slic/dev-versions.py
@@ -8,9 +8,7 @@
 
 
 # --------- ilcsoft release version ------------------------------------------
-ilcsoft_release='dev'
-# ----------------------------------------------------------------------------
-
+ilcsoft_release = "v04-00-00-pre"
 
 # --------- install dir ------------------------------------------------------
 #ilcsoft_install_prefix = "/scratch/$USER/slic/"
@@ -18,12 +16,12 @@ ilcsoft_install_prefix = "/u1/projects/slic"
 
 # ======================= PACKAGE VERSIONS ===================================
 
-XercesC_version = "3.1.1" 
-LCIO_version = "HEAD"
 ILCUTIL_version = "v01-00"
-Geant4_version = "10.00"
-CMake_version = "2.8.6"
+CMake_version = "2.8.12"
+XercesC_version = "3.1.1" 
+Geant4_version = "10.00.p01"
 HepPDT_version = "3.04.01"
+LCIO_version = "HEAD"
 GDML_version = "HEAD"
-LCDD_version="v00-00-01_LCDD-104-dev"
-SLIC_version = "v00-00-01_SLIC-218-dev"  
+LCDD_version = "v04-00-00-pre"
+SLIC_version = "v04-00-00-pre"
