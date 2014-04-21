0a266133cbecfc76ff64d995cbfda0bcae43554c
Jeremy McCormick <jeremym@slac.stanford.edu>
2014-04-21 20:01:20 +0000

    Update GDML tag with major bug fix.  Change Geant4 default to 10.00.p01 from 9.6.p01 version.

--- examples/slic/release-versions.py
+++ examples/slic/release-versions.py
@@ -11,16 +11,16 @@ ilcsoft_release='v03-01-06'
 # ----------------------------------------------------------------------------
 
 # --------- install dir ------------------------------------------------------
-ilcsoft_install_prefix = "/scratch/$USER/slic/"
-#ilcsoft_install_prefix = "/u1/projects/slic/"
+#ilcsoft_install_prefix = "/scratch/$USER/slic/"
+ilcsoft_install_prefix = "/u1/projects/slic/"
 
 # ======================= PACKAGE VERSIONS ===================================
 XercesC_version = "3.1.1" 
 LCIO_version = "v02-04-03"
 ILCUTIL_version = "v01-00"
-Geant4_version = "9.6.p01"
+Geant4_version = "10.00.p01"
 CMake_version = "2.8.5"
 HepPDT_version = "3.04.01"
-GDML_version = "v03-00-02"
+GDML_version = "v03-01-01"
 LCDD_version = "v03-03-04"
 SLIC_version = "v03-01-06"
