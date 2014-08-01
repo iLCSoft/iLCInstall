597387eb67bf7e753606bea4ac8c1731f86a2217
Jeremy McCormick <jeremym@slac.stanford.edu>
2014-08-01 19:41:40 +0000

    Current SLIC release configuration.

--- examples/slic/release-versions.py
+++ examples/slic/release-versions.py
@@ -7,7 +7,7 @@
 ###########################################
 
 # --------- ilcsoft release version ------------------------------------------
-ilcsoft_release='v03-01-06'
+ilcsoft_release='v04-00-00'
 # ----------------------------------------------------------------------------
 
 # --------- install dir ------------------------------------------------------
@@ -18,9 +18,9 @@ ilcsoft_install_prefix = "/u1/projects/slic/"
 XercesC_version = "3.1.1" 
 LCIO_version = "v02-04-03"
 ILCUTIL_version = "v01-00"
-Geant4_version = "10.00.p01"
+Geant4_version = "10.00.p02"
 CMake_version = "2.8.5"
 HepPDT_version = "3.04.01"
 GDML_version = "v03-01-01"
-LCDD_version = "v03-03-04"
-SLIC_version = "v03-01-06"
+LCDD_version = "v04-00-00"
+SLIC_version = "v04-00-00"
