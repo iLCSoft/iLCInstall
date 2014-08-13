6e88df319c8cdf95ca91c6407e9a121cb8df3bf0
Jeremy McCormick <jeremym@slac.stanford.edu>
2014-08-13 19:05:03 +0000

    Update the slicPandora config.  Comment out the dummy Marlin config, at least for now.

--- examples/slicPandora/release-versions.py
+++ examples/slicPandora/release-versions.py
@@ -13,7 +13,7 @@ ilcsoft_release = "v00-00"
 
 
 # --------- install dir ------------------------------------------------------
-ilcsoft_install_prefix = "/scratch/$USER/slicPandora/"
+ilcsoft_install_prefix = "/u1/projects/slicPandora/"
 
 #ilcsoft_install_prefix = "/u1/projects/slic/"
 #ilcsoft_install_prefix = ilcsoft_afs_path[ arch ]
@@ -37,23 +37,20 @@ ilcsoft_install_prefix = "/scratch/$USER/slicPandora/"
 
 # ----- java ---------------------------------------------------------
 #Java_version = "1.6.0"
-Java_version = "1.7.0"
+#Java_version = "1.7.0"
 #Java_path = ilcPath + "/java/" + Java_version # comment out to try auto-detect
 #Java_path = "/usr/"
-Java_path = "/u1/apps/jdk1.7.0_17/"
+#Java_path = "/u1/apps/jdk1.7.0_17/"
 
 LCIO_version = "v02-03-03"
-
 ILCUTIL_version = "v01-00"
-
-CMake_version = "2.8.5"
-
-PandoraPFANew_version = "v00-11"
-
+CMake_version = "2.8.12"
+#PandoraPFANew_version = "v00-11"
+PandoraPFANew_version = "v00-16"
 SlicPandora_version = "HEAD"
 
-# versions for dummy install of Marlin 
-Marlin_version = "HEAD"
-GEAR_version = "v01-03"
-CLHEP_version = "2.1.3.1"
-LCCD_version = "v01-02-01"
+# versions for dummy install of Marlin (why???)
+#Marlin_version = "HEAD"
+#GEAR_version = "v01-03"
+#CLHEP_version = "2.1.3.1"
+#LCCD_version = "v01-02-01"
