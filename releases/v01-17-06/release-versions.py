006a861b5be802ea0f1d24e5337de86204153103
Frank Gaede <frank.gaede@desy.de>
2014-08-19 18:20:58 +0000

    - final update of DD4hep and DDSim versions    vor ============ v01-17-06 =======  - update of the release notes

--- releases/v01-17-06/release-versions.py
+++ releases/v01-17-06/release-versions.py
@@ -166,8 +166,8 @@ SLIC_version = "v04-00-00"
 
 SlicPandora_version = "v01-01-01"
 
-DD4hep_version = "v00-08"
-DDSim_version = "v00-01"
+DD4hep_version = "v00-10"
+DDSim_version = "v00-02"
 
 Physsim_version = "v00-01" 
 
