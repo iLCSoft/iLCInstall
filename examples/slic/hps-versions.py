afac33b488ce913b1b0bffd820ffb6bdcf6a3fa9
Jeremy McCormick <jeremym@slac.stanford.edu>
2015-01-31 01:13:02 +0000

    Add more up to date slic config for HPS.

--- examples/slic/hps-versions.py
+++ examples/slic/hps-versions.py
@@ -7,7 +7,7 @@
 ###########################################
 
 # --------- ilcsoft release version ------------------------------------------
-ilcsoft_release='hps_v01-00-02'
+ilcsoft_release='hps_test'
 # ----------------------------------------------------------------------------
 
 # --------- install dir ------------------------------------------------------
@@ -16,7 +16,7 @@ ilcsoft_release='hps_v01-00-02'
 #ilcsoft_install_prefix = "/scratch/$USER/slic/"
 
 # Jeremy's local config 
-ilcsoft_install_prefix = "/u1/projects/slic/"
+ilcsoft_install_prefix = "/u1/scratch/slic/"
 
 # JLab install dir
 #ilcsoft_install_prefix = "/u/group/hps/hps_soft/slic/"
@@ -27,8 +27,13 @@ CMake_version = "2.8.12.2"
 ILCUTIL_version = "v01-00"
 XercesC_version = "3.1.1" 
 LCIO_version = "v02-04-03"
-Geant4_version = "9.6.p01"
+#Geant4_version = "9.6.p01"
+Geant4_version = "9.6.p03"
 HepPDT_version = "3.04.01"
-GDML_version = "v03-01-01"
-LCDD_version = "v03-03-04"
-SLIC_version = "v03-01-05"
+DDSegmentation_version = "HEAD"
+GDML_version = "HEAD"
+LCDD_version = "HEAD"
+SLIC_version = "HEAD"
+#GDML_version = "v03-01-01"
+#LCDD_version = "v03-03-04"
+#SLIC_version = "v03-01-05"
