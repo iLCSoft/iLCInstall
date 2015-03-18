2becf1d0b74a862bb6ef776c966a6ff637bc6a4e
Frank Gaede <frank.gaede@desy.de>
2015-03-18 14:16:10 +0000

    - fixed name of environment variable

--- ilcsoft/dd4hep_examples.py
+++ ilcsoft/dd4hep_examples.py
@@ -78,12 +78,12 @@ class DD4hepExamples(BaseILC):
     def postCheckDeps(self):
         BaseILC.postCheckDeps(self)
 
-        self.env[ 'DD4hepExamples_DIR' ] = self.installPath
+        self.env[ 'DD4hepExamples' ] = self.installPath
 
 #        self.envcmds.append("export G4WORKDIR=$DD4hepExamples_DIR")
 
         self.envpath["PATH"].append( "$DD4hepExamples/bin" )
-        self.envpath["LD_LIBRARY_PATH"].append( "$DD4hepExamples_DIR/lib" )
+        self.envpath["LD_LIBRARY_PATH"].append( "$DD4hepExamples/lib" )
 
         self.envcmds.append('test -r ${G4ENV_INIT} && { cd $(dirname ${G4ENV_INIT}) ; . ./$(basename ${G4ENV_INIT}) ; cd $OLDPWD ; }')
         
