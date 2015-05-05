8e58f1c4dc0bacaf2c0e9176e6fd3477e47a07a7
Georgios Voutsinas <georgios.voutsinas@desy.de>
2015-05-05 16:58:03 +0000

    Preparation for next ilcsoft release - scripts edited for first testing pre-release

--- releases/v01-17-07/release-versions.py
+++ releases/v01-17-07/release-versions.py
@@ -8,7 +8,7 @@
 
 
 # --------- ilcsoft release version ------------------------------------------
-ilcsoft_release='v01-17-06'
+ilcsoft_release='v01-17-07-pre'
 # ----------------------------------------------------------------------------
 
 
@@ -78,9 +78,9 @@ CMake_version = "2.8.5"
 
 # -------------------------------------------
 
-LCIO_version = "v02-05" 
+LCIO_version = "HEAD" 
 
-GEAR_version = "v01-04-01" 
+GEAR_version = "HEAD" 
 
 CED_version = "v01-09-01"
 
@@ -95,9 +95,13 @@ MarlinFastJet_version = "v00-02"
 
 # -------------------------------------------
 
-KalTest_version = "v01-05-04"
+KalTest_version = "HEAD"
 
-KalDet_version = "v01-13-01"
+KalDet_version = "HEAD"
+
+DDKalTest_version = "HEAD"
+
+aidaTT_version = "HEAD"
 
 GBL_version = "V01-16-04"
 
@@ -109,36 +113,38 @@ MarlinUtil_version = "v01-08-01"
 
 Marlin_version = "v01-05"
 
-Mokka_version = "mokka-08-04" 
+Mokka_version = "HEAD" 
 
-MarlinReco_version = "v01-10"
+MarlinReco_version = "HEAD"
 
-MarlinTrk_version = "v01-11"
+MarlinTrk_version = "HEAD"
 
-MarlinTrkProcessors_version = "v01-11"
+MarlinTrkProcessors_version = "HEAD"
 
-Clupatra_version = "v00-10"
+ILDPerformance_version = "HEAD"
+
+Clupatra_version = "HEAD"
 
 LCFIVertex_version = "v00-06-02"
-LCFIPlus_version = "v00-05-02"
+LCFIPlus_version = "HEAD"
 
-KiTrack_version = "v01-05"
+KiTrack_version = "HEAD"
 KiTrackMarlin_version = "v01-05"
-ForwardTracking_version = "v01-07"
+ForwardTracking_version = "HEAD"
 
-MarlinKinfit_version = "v00-01-02"
+MarlinKinfit_version = "HEAD"
 
-PandoraPFANew_version = "v00-16" #"v00-09"
-MarlinPandora_version = "v00-14" #"v00-09-02"
-PandoraAnalysis_version = "v00-06" #"v00-04"
+PandoraPFANew_version = "HEAD" #"v00-16"
+MarlinPandora_version = "HEAD" #"v00-14"
+PandoraAnalysis_version = "HEAD" #"v00-06"
 
-CEDViewer_version = "v01-07-02"
+CEDViewer_version = "HEAD"
 
 Overlay_version = "v00-14"
 
 PathFinder_version =  "v00-06"
 
-MarlinTPC_version = "v00-17"
+MarlinTPC_version = "HEAD"
 
 LCTuple_version = "v01-03-01"
 
@@ -146,7 +152,7 @@ BBQ_version =  "v00-01-02"
 
 Druid_version = "2.2" # "1.8" 
 
-Garlic_version = "v2.10.1"
+Garlic_version = "HEAD"
 
 
 
@@ -160,16 +166,17 @@ XercesC_version = "3.1.1"
 HepPDT_version = "3.04.01"
 
 # new versions by J. McCormick
-GDML_version = "v03-01-01"
-LCDD_version = "v04-00-00"
-SLIC_version = "v04-00-00"
+GDML_version = "HEAD"
+LCDD_version = "HEAD"
+SLIC_version = "HEAD"
 
-SlicPandora_version = "v01-01-01"
+SlicPandora_version = "HEAD"
 
-DD4hep_version = "v00-10"
-lcgeo_version = "v00-02"
+DD4hep_version = "HEAD"
+DD4hepExamples_version = "HEAD"
+lcgeo_version = "HEAD"
 
-Physsim_version = "v00-01" 
+Physsim_version = "HEAD" 
 
 #--- EUTelescope et al:
 #Eutelescope_version = "trunk" # e.g. "tags/v00-09-00" (checked out via SVN) or "trunk" for git clone of the current dev version
