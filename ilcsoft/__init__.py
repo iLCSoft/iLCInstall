bba811aa5a1dd9ef8dc3385ec6ee04c50ed39fb9
Shaojun Lu <shaojun.lu@desy.de>
2015-05-12 13:15:26 +0000

    Update ilcinstall python script, added gcc481.py to setup the gcc48 path ILCsoft.cmake ans the  environment in init_ilcsoft.sh, and use c++11 option within sl6-c++11_nb_debug.cfg configuration for nightly build.

--- ilcsoft/__init__.py
+++ ilcsoft/__init__.py
@@ -83,4 +83,5 @@ from root import ROOT
 from geant4 import Geant4
 from java import Java
 from mysql import MySQL
-from boost import Boost
+
+from gcc481 import GCC481
---
+++ ilcsoft/gcc481.py
@@ -0,0 +1,37 @@
+##################################################
+#
+# GCC481 module
+#
+# Author: Shaojun Lu, DESY
+# Date: May, 2015
+#
+##################################################
+                                                                                                                                                            
+# custom imports
+from baseilc import BaseILC
+from util import *
+
+
+class GCC481(BaseILC):
+    """ Responsible for the GCC481 configuration process. """
+    
+    def __init__(self, userInput):
+        BaseILC.__init__(self, userInput, "GCC481", "GCC481")
+
+        self.installSupport = False
+
+        self.reqfiles = [
+                ["lib64/libcloog-isl.so"], 
+                ["bin/gcc48"],
+                ["bin/g++48"],
+                ["bin/gfortran48"]
+        ]
+
+
+    def postCheckDeps(self):
+        BaseILC.postCheckDeps(self)
+
+        self.env["LCG_gcc_home"] = self.installPath
+        self.envpath["PATH"].append( "$LCG_gcc_home/bin" )
+        self.envpath["LD_LIBRARY_PATH"].append( "$LCG_gcc_home/lib64" )
+
