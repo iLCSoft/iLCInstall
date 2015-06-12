3aa63a094b6ae75d656d4e6c9e5cc13f3b6503be
Jeremy McCormick <jeremym@slac.stanford.edu>
2015-06-12 21:37:39 +0000

    Development config for slicPandora (currently broken but working on fixing it).

---
+++ examples/slicPandora/slicPandora-head-versions.py
@@ -0,0 +1,33 @@
+###########################################
+#
+# slicPandora HEAD versions
+#
+# Jeremy McCormick, SLAC
+#
+###########################################
+
+# --------- ilcsoft release version ------------------------------------------
+#ilcsoft_release = "v00-00"
+ilcsoft_release = "HEAD"
+# ----------------------------------------------------------------------------
+
+# --------- install dir ------------------------------------------------------
+#ilcsoft_install_prefix = "/u1/projects/slicPandora/"
+
+ilcsoft_install_prefix = "/work/projects/ilcsoft/installations/slicPandora/"
+
+#ilcsoft_install_prefix = "/u1/projects/slic/"
+#ilcsoft_install_prefix = ilcsoft_afs_path[ arch ]
+#ilcsoft_install_prefix = "/scratch/gaede/ilcsoft/"
+#ilcsoft_install_prefix = "/scratch/rosem/ilcsoft/"
+
+#ilcsoft_install_dir = os.path.join( ilcsoft_install_prefix, ilcsoft_release )
+
+# ======================= PACKAGE VERSIONS ===================================
+
+LCIO_version = "v02-03-03"
+ILCUTIL_version = "v01-00"
+CMake_version = "2.8.12"
+#PandoraPFANew_version = "v00-12"
+PandoraPFANew_version = "v00-17"
+SlicPandora_version = "HEAD"
