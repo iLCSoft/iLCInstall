e05238c7424593331a5db515b87fab4e3ee8d561
Jeremy McCormick <jeremym@slac.stanford.edu>
2015-06-15 20:56:34 +0000

    Change slicPandora head build versions.

--- examples/slicPandora/slicPandora-head-versions.py
+++ examples/slicPandora/slicPandora-head-versions.py
@@ -14,7 +14,8 @@ ilcsoft_release = "HEAD"
 # --------- install dir ------------------------------------------------------
 #ilcsoft_install_prefix = "/u1/projects/slicPandora/"
 
-ilcsoft_install_prefix = "/work/projects/ilcsoft/installations/slicPandora/"
+#ilcsoft_install_prefix = "/work/projects/ilcsoft/installations/slicPandora/"
+ilcsoft_install_prefix = "/nfs/slac/g/lcd/mc/prj/users/jeremym/slicPandora/build/"
 
 #ilcsoft_install_prefix = "/u1/projects/slic/"
 #ilcsoft_install_prefix = ilcsoft_afs_path[ arch ]
@@ -25,9 +26,8 @@ ilcsoft_install_prefix = "/work/projects/ilcsoft/installations/slicPandora/"
 
 # ======================= PACKAGE VERSIONS ===================================
 
-LCIO_version = "v02-03-03"
-ILCUTIL_version = "v01-00"
-CMake_version = "2.8.12"
-#PandoraPFANew_version = "v00-12"
-PandoraPFANew_version = "v00-17"
+CMake_version = "3.2.3"
+ILCUTIL_version = "v01-02-01"
+LCIO_version = "v02-04-03"
+PandoraPFANew_version = "v02-00-00"
 SlicPandora_version = "HEAD"
