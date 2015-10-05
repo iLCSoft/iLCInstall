4fff54a973abe0d0b969230efa3d758c5d6fb0ec
Andre Sailer <andre.philippe.sailer@cern.ch>
2015-10-05 08:51:08 +0000

    lcgeo: Add PYTHONPATH export

--- ilcsoft/lcgeo.py
+++ ilcsoft/lcgeo.py
@@ -85,6 +85,7 @@ class lcgeo(BaseILC):
 
         self.envpath["PATH"].append( "$lcgeo_DIR/bin" )
         self.envpath["LD_LIBRARY_PATH"].append( "$lcgeo_DIR/lib" )
+        self.envpath["PYTHONPATH"].append( "$lcgeo_DIR/lib/python" )
 
 #        self.envcmds.append('test -r ${G4ENV_INIT} && { cd $(dirname ${G4ENV_INIT}) ; . ./$(basename ${G4ENV_INIT}) ; cd $OLDPWD ; }')
         
