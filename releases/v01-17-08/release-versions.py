76d2d18ee1b983fc305f8b9231bdae5144819d92
Frank Gaede <frank.gaede@desy.de>
2016-02-09 09:00:09 +0000

    - added optional cvmfs pathes for gcc48    as documentation

--- releases/v01-17-08/release-versions.py
+++ releases/v01-17-08/release-versions.py
@@ -20,6 +20,10 @@ use_cpp11 = False
  source /afs/cern.ch/sw/lcg/external/gcc/4.8.1/x86_64-slc6-gcc48-opt/setup.sh
  export PATH=/afs/cern.ch/sw/lcg/external/Python/2.7.4/x86_64-slc6-gcc48-opt/bin/:$PATH
  export LD_LIBRARY_PATH=/afs/cern.ch/sw/lcg/external/Python/2.7.4/x86_64-slc6-gcc48-opt/lib/:$LD_LIBRARY_PATH
+# or on cvmfs:
+# source /cvmfs/sft.cern.ch/lcg/external/gcc/4.8.1/x86_64-slc6-gcc48-opt/setup.sh
+# export PATH=/cvmfs/sft.cern.ch/lcg/external/Python/2.7.4/x86_64-slc6-gcc48-opt/bin/:$PATH
+# export LD_LIBRARY_PATH=/cvmfs/sft.cern.ch/lcg/external/Python/2.7.4/x86_64-slc6-gcc48-opt/lib/:$LD_LIBRARY_PATH
 '''
 # before starting the installation
 #================================================================================
