ea74a227e8aa41e80a5207db5153dabe5e1de566
Marko Petric <marko.petric@cern.ch>
2016-06-21 22:58:58 +0000

    streamlining for new software chain

--- builds/release-versions-HEAD.py
+++ builds/release-versions-HEAD.py
@@ -10,129 +10,20 @@ import datetime
 
 # --------- ilcsoft release version ------------------------------------------
 today = str( datetime.date.today() )
-ilcsoft_release='HEAD-'+today
+ilcsoft_release='HEAD'
 #ilcsoft_release='HEAD-2016-03-07'
 
-# ----------------------------------------------------------------------------
-
-#-----------------------
-# optionally build with c++11 ?
 use_cpp11 = True
 
-#===============================================================================
-# NB: c++11 - needs a newer compiler and compatible python, e.g. run
-'''
-source /cvmfs/clicdp.cern.ch/compilers/gcc/4.8.5/x86_64-slc6/setup.sh
-source /cvmfs/clicdp.cern.ch/software/CMake/3.5.2/x86_64-slc6-gcc48-opt/setup.sh
-'''
-# export PATH=/afs/cern.ch/sw/lcg/external/Python/2.7.4/x86_64-slc6-gcc48-opt/bin/:$PATH
-# export LD_LIBRARY_PATH=/afs/cern.ch/sw/lcg/external/Python/2.7.4/x86_64-slc6-gcc48-opt/lib/:$LD_LIBRARY_PATH
-# or on cvmfs:
-# source /cvmfs/sft.cern.ch/lcg/external/gcc/4.8.1/x86_64-slc6-gcc48-opt/setup.sh
-# export PATH=/cvmfs/sft.cern.ch/lcg/external/Python/2.7.4/x86_64-slc6-gcc48-opt/bin/:$PATH
-# export LD_LIBRARY_PATH=/cvmfs/sft.cern.ch/lcg/external/Python/2.7.4/x86_64-slc6-gcc48-opt/lib/:$LD_LIBRARY_PATH
-#'''
-# before starting the installation
-#================================================================================
-
-
-
-# --------- install dir ------------------------------------------------------
-# ===========================================================
-# Modify this path to where you want to install the software
-# ===========================================================
-#
-
-#ilcsoft_install_prefix = ilcsoft_afs_path[ arch ]
-#ilcsoft_install_prefix = "/afs/desy.de/project/ilcsoft/sw/x86_64_gcc44_sl6/"
-#ilcsoft_install_prefix = "/nfs/dust/ilc/user/voutsina/testarea/ilcsoft_c11/"
 ilcsoft_install_prefix = "/scratch/ilcsoft/"
-
-# ----------------------------------------------------------------------------
-#--- the ilcsoft_release is now automatically appended in release-ilcsoft.cfg 
-#     but not in release-base.cfg !!
-
-#append_version_to_install_prefix = False
-#if(append_version_to_install_prefix):
-#    ilcsoft_install_dir = os.path.join(ilcsoft_install_prefix , ilcsoft_release )
-
-# ----------------------------------------------------------------------------
-
-
-# --------- ilcsoft home -----------------------------------------------------
-# ===========================================================
-# Modify this path to where you want ilcinstall to look
-# for pre-installed (base) packages
-# typically this would be left to ilcsoft_install_prefix
-# or set to an /afs or /cvmfs base installation that you 
-# want to use
-# ===========================================================
-
-#ilcPath = '/afs/desy.de/project/ilcsoft/sw/x86_64_gcc44_sl6'
-#ilcPath = '/afs/desy.de/project/ilcsoft/sw/x86_64_gcc48_sl6'
-#ilcPath = '/afs/desy.de/project/ilcsoft/sw/x86_64_gcc41_sl5'
 
 
 ilcPath = ilcsoft_install_prefix
-# ----------------------------------------------------------------------------
 
-
-#--------------------------------------------------------------------------
-#ilcPatchPath = "/afs/desy.de/project/ilcsoft/sw/x86_64_gcc41_sl5/v01-15"
-
-
-
-# ======================= PACKAGES WITH NO INSTALL SUPPORT ===================
-
-# these packages need to be pre-installed for your system
-# please adjust the path variables accordingly
-
-# ----- mysql --------------------------------------------------------
-#MySQL_version = "5.0.45"
-#MySQL_path = ilcPath + "/mysql/" + MySQL_version
-
-#if( ilcsoft_afs_path[ arch ] == '/afs/desy.de/project/ilcsoft/sw/x86_64_gcc46_ub1204' ):
-#MySQL_path = "/usr"
-
-
-#------ boost headers files ------------------------------------------
 Boost_path = "/cvmfs/clicdp.cern.ch/software/Boost/1.61.0/x86_64-slc6-gcc48-opt/"
-
-
-# ----- CERNLIB ------------------------------------------------------
-CERNLIB_version = "2006" 
-CERNLIB_path = "/afs/desy.de/project/ilcsoft/sw/x86_64_gcc44_sl6/cernlib/" + CERNLIB_version
-
-
-
-# ----------------------------------------------------------------------------
-
-Fortran_lib_path = ""
-# ----- when using gcc48 we need to give a hint where to find the libary:
-if( use_cpp11 ):
-    Fortran_lib_path = "/afs/cern.ch/sw/lcg/contrib/gcc/4.8.1/x86_64-slc6-gcc48-opt/lib64"
-
-
-##########################################################################################
-#
-#  end of user configuration section
-#  only make changes below if you know what you are doing ...
-#
-##########################################################################################
-
-
-
-#=============================================================================
-# CXX_FLAGS for c++ compiler:
-#
 
 if( use_cpp11 ):
     CMAKE_CXX_FLAGS = '-Wall -std=c++11'
-else:
-    CMAKE_CXX_FLAGS = '-Wall -ansi -pedantic -Wno-long-long'
-
-# ----------------------------------------------------------------------------
-
 
 # ======================= PACKAGE VERSIONS ===================================
 
