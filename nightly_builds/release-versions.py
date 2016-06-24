d443ddb6187e059230e5571163068d369827e42c
Georgios Voutsinas <georgios.voutsinas@desy.de>
2016-06-24 16:23:31 +0000

    updating the nightly builds scripts

--- nightly_builds/release-versions.py
+++ nightly_builds/release-versions.py
@@ -1,38 +1,84 @@
 ###########################################
 #
-# iLCSoft versions for release v01-17-07
+# iLCSoft versions for installing a current HEAD 
+# version of the ilcsoft packages.
+# The external base tools need to be installed
 #
 # DESY ilcsoft team
 ###########################################
-
+import datetime
 
 # --------- ilcsoft release version ------------------------------------------
-ilcsoft_release='v01-17-07-pre00'
+today = str( datetime.date.today() )
+ilcsoft_release='HEAD-'+today
+#ilcsoft_release='HEAD-2016-03-07'
+
 # ----------------------------------------------------------------------------
+
+#-----------------------
+# optionally build with c++11 ?
+use_cpp11 = False #True 
+
+#===============================================================================
+# NB: c++11 - needs a newer compiler and compatible python, e.g. run
+'''
+ source /afs/cern.ch/sw/lcg/external/gcc/4.8.1/x86_64-slc6-gcc48-opt/setup.sh
+ export PATH=/afs/cern.ch/sw/lcg/external/Python/2.7.4/x86_64-slc6-gcc48-opt/bin/:$PATH
+ export LD_LIBRARY_PATH=/afs/cern.ch/sw/lcg/external/Python/2.7.4/x86_64-slc6-gcc48-opt/lib/:$LD_LIBRARY_PATH
+# or on cvmfs:
+# source /cvmfs/sft.cern.ch/lcg/external/gcc/4.8.1/x86_64-slc6-gcc48-opt/setup.sh
+# export PATH=/cvmfs/sft.cern.ch/lcg/external/Python/2.7.4/x86_64-slc6-gcc48-opt/bin/:$PATH
+# export LD_LIBRARY_PATH=/cvmfs/sft.cern.ch/lcg/external/Python/2.7.4/x86_64-slc6-gcc48-opt/lib/:$LD_LIBRARY_PATH
+'''
+# before starting the installation
+#================================================================================
+
 
 
 # --------- install dir ------------------------------------------------------
-#ilcsoft_install_prefix = "/scratch/$USER/ilcsoft/"
-ilcsoft_install_prefix = ilcsoft_afs_path[ arch ]
-#ilcsoft_install_prefix = "/space/ilcsoft/"
-#ilcsoft_install_prefix = "/scratch/rosem/ilcsoft/"
-ilcsoft_install_prefix = "/afs/desy.de/project/ilcsoft/sw/x86_64_gcc48_sl5/"
+# ===========================================================
+# Modify this path to where you want to install the software
+# ===========================================================
+#
 
-#ilcsoft_install_dir = os.path.join( ilcsoft_install_prefix, ilcsoft_release )
+ilcsoft_install_prefix = ilcsoft_afs_path[ arch ]
+#ilcsoft_install_prefix = "/afs/desy.de/project/ilcsoft/sw/x86_64_gcc44_sl6/"
+#ilcsoft_install_prefix = "/nfs/dust/ilc/user/voutsina/testarea/ilcsoft_c11/"
+#ilcsoft_install_prefix = "/scratch/ilcsoft/"
+
+# ----------------------------------------------------------------------------
+#--- the ilcsoft_release is now automatically appended in release-ilcsoft.cfg 
+#     but not in release-base.cfg !!
+
+#append_version_to_install_prefix = False
+#if(append_version_to_install_prefix):
+#    ilcsoft_install_dir = os.path.join(ilcsoft_install_prefix , ilcsoft_release )
+
 # ----------------------------------------------------------------------------
 
 
 # --------- ilcsoft home -----------------------------------------------------
-# python variable for referring the ILC Home directory
-# used to link or use already installed packages (SL4 or SL5)
-# no need to set this variable if using SL4 or SL5 with access to /afs/desy.de/
+# ===========================================================
+# Modify this path to where you want ilcinstall to look
+# for pre-installed (base) packages
+# typically this would be left to ilcsoft_install_prefix
+# or set to an /afs or /cvmfs base installation that you 
+# want to use
+# ===========================================================
+
 #ilcPath = '/afs/desy.de/project/ilcsoft/sw/x86_64_gcc44_sl6'
+#ilcPath = '/afs/desy.de/project/ilcsoft/sw/x86_64_gcc48_sl6'
 #ilcPath = '/afs/desy.de/project/ilcsoft/sw/x86_64_gcc41_sl5'
-#ilcPath = ilcsoft_afs_path[ arch ]
+
+
 ilcPath = ilcsoft_install_prefix
 # ----------------------------------------------------------------------------
 
+
+#--------------------------------------------------------------------------
 #ilcPatchPath = "/afs/desy.de/project/ilcsoft/sw/x86_64_gcc41_sl5/v01-15"
+
+
 
 # ======================= PACKAGES WITH NO INSTALL SUPPORT ===================
 
@@ -42,156 +88,186 @@ ilcPath = ilcsoft_install_prefix
 # ----- mysql --------------------------------------------------------
 MySQL_version = "5.0.45"
 MySQL_path = ilcPath + "/mysql/" + MySQL_version
-#MySQL_path = "/usr"
 
+if( ilcsoft_afs_path[ arch ] == '/afs/desy.de/project/ilcsoft/sw/x86_64_gcc46_ub1204' ):
+    MySQL_path = "/usr"
+
+
+#------ boost headers files ------------------------------------------
 Boost_path = "/afs/desy.de/project/ilcsoft/sw/boost/1.58.0"
-
-
-
-
-
-
-
-# ----- java ---------------------------------------------------------
-Java_version = "1.6.0"
-Java_path = ilcPath + "/java/" + Java_version # comment out to try auto-detect
 
 
 # ----- CERNLIB ------------------------------------------------------
 CERNLIB_version = "2006" 
-CERNLIB_path = ilcPath + "/cernlib/" + CERNLIB_version
+CERNLIB_path = "/afs/desy.de/project/ilcsoft/sw/x86_64_gcc44_sl6/cernlib/" + CERNLIB_version
 
 
-# ----- if fortran is needed give a hint where to find the libary
-Fortran_lib_path = "/afs/cern.ch/sw/lcg/contrib/gcc/4.8.1/x86_64-slc6-gcc48-opt/lib64"
+
+# ----------------------------------------------------------------------------
+
+Fortran_lib_path = ""
+# ----- when using gcc48 we need to give a hint where to find the libary:
+if( use_cpp11 ):
+    Fortran_lib_path = "/afs/cern.ch/sw/lcg/contrib/gcc/4.8.1/x86_64-slc6-gcc48-opt/lib64"
 
 
+##########################################################################################
+#
+#  end of user configuration section
+#  only make changes below if you know what you are doing ...
+#
+##########################################################################################
+
+
+
+#=============================================================================
+# CXX_FLAGS for c++ compiler:
+#
+
+#if( use_cpp11 ):
+#    CMAKE_CXX_FLAGS = '-Wall -std=c++11'
+#else:
+#  CMAKE_CXX_FLAGS = '-Wall -ansi -pedantic -Wno-long-long'
+
+
+# ----------------------------------------------------------------------------
 
 
 # ======================= PACKAGE VERSIONS ===================================
 
-Geant4_version = "10.01"  #"9.5.p02" 
-#version 10.00 is on disk
+Geant4_version =  "10.01" # "10.02.p01" # "10.01" 
 
-ROOT_version = "5.34.18" 
+if( use_cpp11 ):
+    ROOT_version = "6.06.02"
+else:
+    ROOT_version = "5.34.30" 
 
-CLHEP_version = "2.1.4.1" #already on disk -- needed for geant4.10
-#CLHEP_version = "2.1.1.0" # needed for g4 9.5   # ----- "2.1.3.1"
+CLHEP_version =  "2.1.4.1" # "2.3.1.1" #  "2.1.4.1"
 
-GSL_version = "1.14"
+GSL_version = "2.1" # "1.14"
 
 QT_version = "4.7.4"
 
-CMake_version = "3.4.3" #"2.8.5"
-
-
-# -------------------------------------------
-
-LCIO_version = "HEAD" 
-
-GEAR_version = "HEAD" 
+CMake_version = "3.4.3" # "2.8.5"
 
 CED_version = "v01-09-01"
 
+# -------------------------------------------
+
+LCIO_version = "HEAD" # "v02-06"
+
+GEAR_version = "HEAD" # "v01-04-02" 
+
 CondDBMySQL_version = "CondDBMySQL_ILC-0-9-6"
 
-ILCUTIL_version = "v01-02-01" 
+ILCUTIL_version = "v01-03-pre"  #  "v01-02-01" 
 
 FastJet_version = "3.1.2"
 
-FastJetClustering_version = "v00-02"
+FastJetClustering_version = "HEAD" #"v00-02"
 
-MarlinFastJet_version = "v00-02"
+MarlinFastJet_version = "HEAD" # "v00-02"
 
 
 # -------------------------------------------
 
-KalTest_version = "HEAD"
+KalTest_version = "HEAD" # "v02-00"  
 
-KalDet_version = "HEAD"
+KalDet_version = "HEAD" # "v01-13-02"
 
-DDKalTest_version = "HEAD"
+aidaTT_version = "HEAD" # "v00-02"
 
-aidaTT_version = "HEAD"
+DDKalTest_version = "HEAD" # "v00-02"
 
-GBL_version = "V01-16-04"
+MarlinTrk_version = "HEAD" # "v02-00-01"
 
-LCCD_version = "v01-03"
+MarlinTrkProcessors_version = "HEAD" # "v02-01"
 
-RAIDA_version = "v01-06-02"
+Clupatra_version = "HEAD" # "v00-12"
 
-MarlinUtil_version = "v01-08-01"
+KiTrack_version = "HEAD" # "v01-06"
 
-Marlin_version = "v01-06-pre"
+KiTrackMarlin_version = "HEAD" # "v01-07"
 
-MarlinDD4hep_version = "HEAD"
+ForwardTracking_version = "HEAD" # "v01-08"
 
-Mokka_version = "HEAD" 
+# -------------------------------------------
 
-MarlinReco_version = "HEAD"
+GBL_version = "HEAD" # "V01-16-04"
 
-MarlinTrk_version = "HEAD"
+LCCD_version = "HEAD" # "v01-03"
 
-MarlinTrkProcessors_version = "HEAD"
+RAIDA_version = "HEAD" # "v01-06-02"
 
-ILDPerformance_version = "HEAD"
+MarlinUtil_version = "HEAD" # "v01-10"
 
-Clupatra_version = "HEAD"
+Marlin_version = "HEAD" # "v01-07"
 
-LCFIVertex_version = "v00-06-02"
-LCFIPlus_version = "HEAD"
+MarlinDD4hep_version = "HEAD" # "v00-01"
 
-KiTrack_version = "HEAD"
-KiTrackMarlin_version = "v01-05"
-ForwardTracking_version = "HEAD"
+DDMarlinPandora_version = "HEAD" # "v00-01"
 
-MarlinKinfit_version = "HEAD"
+Mokka_version = "HEAD" # "mokka-08-05" 
 
-PandoraPFANew_version = "HEAD" #"v00-16"
-MarlinPandora_version = "HEAD" #"v00-14"
-PandoraAnalysis_version = "HEAD" #"v00-06"
+MarlinReco_version = "HEAD" # "v01-13"
 
-CEDViewer_version = "HEAD"
+FCalClusterer_version = "HEAD" # "v00-01"
 
-Overlay_version = "v00-14"
+ILDPerformance_version = "HEAD" # "v00-01"
 
-PathFinder_version =  "v00-06"
-
-MarlinTPC_version = "HEAD"
-
-LCTuple_version = "v01-03-01"
-
-BBQ_version =  "v00-01-02"
-
-Druid_version = "2.2" # "1.8" 
-
-Garlic_version = "HEAD"
+ILDConfig_version = "HEAD" 
 
 
+LCFIVertex_version = "HEAD" # "v00-07"
+LCFIPlus_version = "HEAD" # "v00-05-03"
 
-#--- slic et al:
+
+MarlinKinfit_version = "HEAD" # "v00-01-05"
+
+PandoraPFANew_version = "HEAD" # "v02-00-00"
+MarlinPandora_version = "HEAD" # "v02-00-00"
+PandoraAnalysis_version = "HEAD" # "v01-00-01" 
+
+CEDViewer_version = "HEAD" # "v01-10"
+
+Overlay_version = "HEAD" # "v00-14"
+
+PathFinder_version = "HEAD" #  "v00-06"
+
+MarlinTPC_version = "HEAD" # "v01-00"
+
+LCTuple_version = "HEAD" # "v01-04"
+
+BBQ_version = "HEAD" #  "v00-01-02"
+
+Druid_version = "HEAD" # "2.2" # "1.8" 
+
+Garlic_version = "HEAD" # "v3.0.3"
+
+DD4hep_version = "HEAD" # "v00-14"
+
+DD4hepExamples_version = "HEAD" # "v00-14"
+
+lcgeo_version = "HEAD" # "v00-05"
+
+Physsim_version = "HEAD" # "v00-02" 
+
 
 # xerces-c (needed by geant4 for building gdml support - required by mokka)
 XERCESC_ROOT_DIR = ilcPath + "/xercesc/3.1.2"
 
 XercesC_version = "3.1.2" 
 
-HepPDT_version = "3.04.01"
+#--- slic et al:
 
-# new versions by J. McCormick
-GDML_version = "HEAD"
-LCDD_version = "HEAD"
-SLIC_version = "HEAD"
 
-SlicPandora_version = "HEAD"
+#HepPDT_version = "3.04.01"
 
-DD4hep_version = "HEAD"
-DD4hepExamples_version = "HEAD"
-lcgeo_version = "HEAD"
+# versions tagged by J.Strube for this release 
+#GDML_version = "ilcsoft-v01-17-07"
+#LCDD_version = "ilcsoft-v01-17-07"
+#SLIC_version = "ilcsoft-v01-17-07"
+#SlicPandora_version = "ilcsoft-v01-17-07"
 
-Physsim_version = "HEAD" 
 
-#--- EUTelescope et al:
-#Eutelescope_version = "trunk" # e.g. "tags/v00-09-00" (checked out via SVN) or "trunk" for git clone of the current dev version
-#Eudaq_version = "trunk" # e.g. "tags/v1.2.2" (checked out via SVN) or "trunk" for a full git clone of the current development version
-#Millepede2_version = 'trunk'
+
