e90c1cb4edcabb7dc4ecc82d0b9d71de1e044ce8
Georgios Voutsinas <georgios.voutsinas@desy.de>
2016-09-19 13:41:45 +0000

    adding a small script to group the setting of the env. for the kronjob for the gcc48 nighlty builds

---
+++ nightly_builds/build_sl6_gcc48_env.sh
@@ -0,0 +1,3 @@
+source /afs/cern.ch/sw/lcg/external/gcc/4.8.1/x86_64-slc6-gcc48-opt/setup.sh 
+export PATH=/afs/cern.ch/sw/lcg/external/Python/2.7.4/x86_64-slc6-gcc48-opt/bin/:$PATH  
+export LD_LIBRARY_PATH=/afs/cern.ch/sw/lcg/external/Python/2.7.4/x86_64-slc6-gcc48-opt/lib/:$LD_LIBRARY_PATH
