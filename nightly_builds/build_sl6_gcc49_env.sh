a8961dc9a2bb2e5369d17eb8aef184af8196b792
Shaojun Lu <shaojun.lu@desy.de>
2017-02-03 09:37:05 +0000

    Added one script for gcc4.9.3 environment setup.

---
+++ nightly_builds/build_sl6_gcc49_env.sh
@@ -0,0 +1,5 @@
+source /afs/cern.ch/sw/lcg/contrib/gcc/4.9.3/x86_64-slc6/setup.sh
+export PATH=/afs/cern.ch/sw/lcg/releases/LCG_87/Python/2.7.10/x86_64-slc6-gcc49-opt/bin:$PATH
+export LD_LIBRARY_PATH=/afs/cern.ch/sw/lcg/releases/LCG_87/Python/2.7.10/x86_64-slc6-gcc49-opt/lib:$LD_LIBRARY_PATH
+export CC=gcc
+export CXX=g++
