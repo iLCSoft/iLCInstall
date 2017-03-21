23d450b32bfac2c41f1fa597d4db9aa62448758e
Shaojun Lu <shaojun.lu@desy.de>
2017-03-21 09:53:42 +0000

    Moved CEDViewer to GitHub.

--- ilcsoft/cedviewer.py
+++ ilcsoft/cedviewer.py
@@ -23,7 +23,10 @@ class CEDViewer(MarlinPKG):
         #self.optmodules = [ "LCCD", "MarlinTPC" ]
 
         # cvs root
-        self.download.root="marlinreco"
+        #self.download.root="marlinreco"
+        self.download.supportedTypes = [ "GitHub" ] 
+        self.download.gituser = 'iLCSoft'
+        self.download.gitrepo = 'CEDViewer'
 
 
     def postCheckDeps(self):
