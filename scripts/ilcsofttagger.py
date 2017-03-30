a23c0c9be209ffc6d5c327e8bfc6ee95f64f3c2a
Andre Sailer <andre.philippe.sailer@cern.ch>
2017-03-30 16:58:43 +0200

    Add ilcsofttagging script to create releases for github packages
    usage:
    
    ilcsofttagger --packages lcgeo marlin aidasoft/dd4hep fcalsw/fcalclusterer ... [--properRelease] [--makeTags] [--file packageFile]
    
    * default is dry run mode use --makeTags to actually make the tags
    
    * default is pre-release tags, use --properRelease to make real release
    
    * The script will parse github pull requests to collate release notes
      and write them into doc/ReleaseNotes.md
    
    * The script will update the _VERSION_MAJOR, MINOR, PATH in the base
      CMakeLists.txt file to the new version
    
    * One needs a github personal access token in a file called
      GitTokens.py in a constant called GITHUBTOKEN, see helperfunctions
      for details, also the ImportError will give this information
    
    * Will not make another prerelease tag if there are no new commits
      since the last tag Release notes are always written for the new
      version
    
    * to set the version number give the repo onwer (ilcsoft) and the
      version number afterwards ilcsofttagger --packages
      ilcsoft/lcgeo/v10-00 ... [--properRelease] [--makeTags]
    
    * can also use a configuration file which contains the same kind of
      strings
    
        ilcsofttagger --file configFile
    
    * see ilcsofttagger --help as well

---
+++ scripts/ilcsofttagger.py
@@ -0,0 +1,231 @@
+#!/bin/env python
+"""
+
+Tagging System for iLCSoft libraries. Make tags, collate release notes via command line or steering file
+
+
+owner, repository, branch, tag (optional), createReleaseNotes(bool), prerelease(bool)
+
+"""
+
+from logging import getLogger
+import logging
+from pprint import pprint
+
+import argparse
+
+__RCSID__ = None
+
+from tagging.gitinterface import Repo
+from tagging.helperfunctions import checkRate
+
+def _parsePrintLevel( level ):
+  """ translate printlevel to logging level"""
+  return dict( CRITICAL=logging.CRITICAL,
+               ERROR=logging.ERROR,
+               WARNING=logging.WARNING,
+               INFO=logging.INFO,
+               DEBUG=logging.DEBUG,
+             )[level]
+
+class ILCSoftTagger(object):
+  """ object to hold the interface to make tags, read command line options, etc"""
+
+  def __init__( self ):
+    self.configFile = None
+    self.printLevel = "INFO"
+    self.packages = []
+    self.repos = []
+    self.makeTags = False
+    self.properRelease = False
+    self.errors = []
+    self.log = getLogger( "Tagger" )
+
+  def abort( self ):
+    """ print error messages and raise exception """
+    for line in self.errors:
+      self.log.error( "ERROR: %s", line )
+    raise RuntimeError( "ERROR" )
+
+  def parseOptions(self):
+    """parse the command line options"""
+    parser = argparse.ArgumentParser("Makking ILCSoft Tags",
+                                     formatter_class=argparse.RawTextHelpFormatter)
+
+    parser.add_argument("--file", action="store", default=self.configFile, dest="configFile",
+                        help="file holding packages to tag")
+
+    parser.add_argument("-v", "--printLevel", action="store", default=self.printLevel, dest="printLevel",
+                        choices=('DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'),
+                        help="Verbosity DEBUG, INFO, WARNING, ERROR, CRITICAL")
+
+    parser.add_argument("--packages", action="store", default=self.packages, nargs='+',
+                        help="packages to check: [owner/]package[/version[/branch]]")
+
+    parser.add_argument("--makeTags", action="store_true", dest="makeTags", default=self.makeTags,
+                        help="Create the tags, commit release notes")
+
+    parser.add_argument("--properRelease", action="store_true", dest="properRelease", default=self.properRelease,
+                        help="if set make proper release, if not set make pre release only")
+
+
+    parser.add_argument("--checkRate", action="store_true", dest="checkRate", default=False,
+                        help="Print out remaining number of queries for this hour")
+
+
+    parsed = parser.parse_args()
+
+    self.configFile = parsed.configFile
+    self.printLevel = _parsePrintLevel( parsed.printLevel )
+    logging.basicConfig( level=self.printLevel, format='%(levelname)-5s - %(name)-8s: %(message)s' )
+
+    self.packages = parsed.packages
+    self.makeTags = parsed.makeTags
+    self.properRelease = parsed.properRelease
+
+    self._parseConfigFile()
+
+    if parsed.checkRate:
+      checkRate()
+
+    if self.errors:
+      self.abort()
+
+  def _printVersionsFile( self ):
+    """ print the versions.cfg file to be used in ilcsoft-install config files"""
+    self.log.info( "New version configuration:")
+    versionsContent = ""
+    for package in self.repos:
+      versionsContent += '\n%s_version = "%s" ' % ( package.repo, package.newTag )
+
+    print "*"*80
+    print versionsContent
+    print "*"*80
+
+
+  def _parseConfigFile( self ):
+    """ parses the config file and files packages list"""
+    if self.configFile is None:
+      return
+
+
+    with open(self.configFile) as conf:
+      for line in conf.readlines():
+        if not line.strip():
+          continue
+        parts = line.strip().split(" ")
+        if len(parts) != 1:
+          self.errors.append( "Line '%s' cannot be parsed" % line.strip() )
+        else:
+          self.packages.append( line.strip() )
+
+    return
+
+
+  def parsePackage( self, packageString ):
+    """ return tuple of owner, repo, branch, version
+
+    default owner: ilcsoft
+    default branch: master
+    default version: None
+    """
+    slashCount = packageString.count('/')
+    if slashCount == 0:
+      owner = "iLCSoft"
+      package = packageString
+      version=None
+      branch='master'
+    elif slashCount == 1:
+      owner = packageString.split('/')[0]
+      package = packageString.split('/')[1]
+      version=None
+      branch='master'
+    elif slashCount == 2:
+      owner = packageString.split('/')[0]
+      package = packageString.split('/')[1]
+      version = packageString.split('/')[2]
+      branch='master'
+    elif slashCount == 3:
+      owner = packageString.split('/')[0]
+      package = packageString.split('/')[1]
+      version = packageString.split('/')[2]
+      branch = packageString.split('/')[3]
+
+    self.log.debug( "Added package: %s %s %s %s", owner, package, branch, version )
+    return owner, package, branch, version
+
+  def _printReleaseNotes( self ):
+    """ print release notes for all packages """
+    for thisPackage in self.repos:
+      if thisPackage.isUpToDate():
+        continue
+      separateString = "*"*80
+      if self.makeTags:
+        self.log.info( "Create new release for %s: %s", thisPackage.repo, thisPackage.newTag )
+      else:
+        self.log.info( "Dry Run for new %s release: %s", thisPackage.repo, thisPackage.newTag )
+      self.log.info( "Release notes for %s:\n%s\n%s\n%s",
+                     thisPackage,
+                     separateString,
+                     thisPackage.formatReleaseNotes(),
+                     separateString, )
+
+
+  def _commitAndTag( self ):
+    """ commit release notes and updated versions settings and make release """
+    for package in self.repos:
+      if package.isUpToDate():
+        self.log.info( "Package %s has no updates since last tag", package )
+        continue
+      self.log.info( "Committing release notes for: %s", package )
+      package.commitNewReleaseNotes()
+      self.log.info( "Updating version: %s to %s", package, package.newVersion )
+      package.updateVersionSettings()
+      package.createGithubRelease()
+
+
+  def run( self ):
+    """ execute everything """
+    for package in self.packages:
+      self.log.info( "Checking package: %s", package )
+      owner, package, branch, version = self.parsePackage( package )
+      prerelease = not self.properRelease
+      try:
+        dryRun = not self.makeTags
+        thisPackage = Repo( owner, package, branch=branch, newVersion=version, preRelease=prerelease, dryRun=dryRun)
+      except RuntimeError as e:
+        self.log.error( "Failure for %s", thisPackage )
+        self.errors.append( str(e) )
+      self.repos.append( thisPackage )
+
+    if self.errors:
+      self.abort()
+      
+    self._printReleaseNotes()
+    self._commitAndTag()
+
+    
+    self._printVersionsFile()
+    checkRate()
+
+
+if __name__ == "__main__":
+  #logging.basicConfig( level=logging.INFO)
+  RUNNER = ILCSoftTagger()
+  try:
+    RUNNER.parseOptions()
+  except RuntimeError as e:
+    print ("Error during runtime: %s", e)
+    exit(1)
+
+  try:
+    RUNNER.run()
+  except RuntimeError as e:
+    logging.error("Error during runtime: %s", e)
+    exit(1)
+
+
+### Get list of tags, get latest tag, get all PRs merged after this date, get
+### comments for this PR, parse comments for "Relase Notes" and collate them
+### into release notes for the pacakge, add commit with release notes, add
+### commit with new version, ignore pre-release tags for collating release notes
---
+++ scripts/tagging/helperfunctions.py
@@ -0,0 +1,143 @@
+""" helper functions for the gitinterface """
+
+import json
+import logging
+import subprocess
+import re
+try:
+  from GitTokens import GITHUBTOKEN
+except ImportError:
+  raise ImportError("""Failed to import GITHUBTOKEN please point the pythonpath to your GitTokens.py file which contains four "Personal Access Token" for Github
+
+                    I.e.:
+                    Filename: GitTokens.py
+                    Content:
+                    ```
+                    GITHUBTOKEN = "e0b83063396fc632646603f113437de9"
+                    ```
+                    (without the triple quotes)
+                    """
+                   )
+
+
+def versionComp( v1, v2 ):
+  """ compare version strings, problem is comparing
+  v1r8p0 to v02-07-00 e.g. in lcio otherwise we could use normal string comparison
+
+  needed for LCIO
+  """
+  if v1 == v2:
+    return 0
+
+  pattern = re.compile( 'v([0-9]+)[r]([0-9]+)[-p]([0-9]+)')
+
+  result1 = re.match( pattern, v1 )
+
+  result2 = re.match( pattern, v2 )
+
+  ## matching result1
+  if result1 is not None and result2 is not None:
+    ## both match, we can use string comparison
+    pass
+  elif result1 is not None and result2 is None:
+    ## only 1 matches, 2 is always assumed to be bigger
+    return -1
+  elif result1 is None and result2 is not None:
+    ## only 2 matches, 1 is considered bigger
+    return 1
+  else:
+    ## neither matches, we can use string comparison
+    pass
+  
+  if v1 < v2:
+    return -1
+  else:
+    return 1
+  
+
+def parseForReleaseNotes( commentBody ):
+  """ will look for "BEGINRELEASENOTES / ENDRELEAENOTES" and extend releaseNoteList if there are entries """
+
+  relNotes = []
+  if not all( tag in commentBody for tag in ("BEGINRELEASENOTES", "ENDRELEASENOTES") ):
+    return relNotes
+
+  releaseNotes=commentBody.split("BEGINRELEASENOTES")[1].split("ENDRELEASENOTES")[0]
+  relNotes.append( releaseNotes )
+  # for entry in releaseNotes.split("*"):
+  #   if entry.strip():
+  #     relNotes.append( entry.strip() )
+
+  return relNotes
+
+def ghHeaders( ):
+  """ return authorization header for github as used by curl
+
+  :returns: tuple to be used in commands list
+  """
+  return '-H','Authorization: token %s' % GITHUBTOKEN
+
+
+def getCommands( *args ):
+  """ create a flat list
+
+  :param *args: list of strings or tuples/lists
+  :returns: flattened list of strings
+  """
+  comList = []
+  for arg in args:
+    if isinstance( arg, (tuple, list) ):
+      comList.extend(arg)
+    else:
+      comList.append(arg)
+  return comList
+
+
+def curl2Json( *commands, **kwargs ):
+  """ return the json object from calling curl with the given commands
+
+  :param *commands: list of strings or tuples/lists, will be passed to `getCommands` to be flattend
+  :returns: json object returned from the github or gitlab API
+  """
+  log = logging.getLogger("GitHub")
+  commands = getCommands( *commands )
+  commands.insert( 0, 'curl' )
+  commands.insert( 1, '-s' )
+  if kwargs.get("checkStatusOnly", False):
+    commands.insert( 1, '-I' )
+  log.debug( "Running command: %r", commands )
+  jsonText = subprocess.check_output( commands )
+  try:
+    jsonList = json.loads( jsonText )
+  except ValueError:
+    if kwargs.get("checkStatusOnly", False):
+      return jsonText
+    raise
+  return jsonList
+
+
+
+AUTHORMAP = {}
+def authorMapping( username, commands ):
+  """return the name of the author for given username, if not found query github
+  PR for author via the commands given
+  """
+  log = logging.getLogger("Author")
+  author = AUTHORMAP.get(username)
+  if author is not None:
+    log.debug( "Found author in map: %s", author)
+    return author
+
+  log.debug( "Checking Commits for Author ")
+  commits = curl2Json( commands )
+  author = commits[-1]['commit']['author']['name'] ## use the last commit of PR to get author
+  AUTHORMAP[username] = author
+  log.debug( "Found author: %s", author )
+  return author
+
+
+def checkRate():
+  """ return the result for check_rate call """
+  rate = curl2Json( ghHeaders(), "https://api.github.com/rate_limit" )
+  logging.getLogger("Rate").info("Remaining calls to github API are %s of %s", rate['rate']['remaining'], rate['rate']['limit'] )
+
