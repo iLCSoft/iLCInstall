1359957fa01d790e7cabadaf520a3d83a7241d68
Remi Ete <eteremi@flc03.desy.de>
2019-10-09 14:32:37 +0200

    Eigen module now downloads and install Eigen instead of using system one

--- ilcsoft/eigen.py
+++ ilcsoft/eigen.py
@@ -20,13 +20,40 @@ class Eigen(BaseILC):
     def __init__(self, userInput):
         BaseILC.__init__(self, userInput, "Eigen", "eigen")
 
-        self.installSupport = False
-        self.hasCMakeBuildSupport = False
+        # technically possible but set this to false
+        self.download.supportHEAD = False
+        self.hasCMakeBuildSupport = True
+        
+        self.reqmodules = [ "CMake" ]
+        self.download.supportedTypes = [ "GitHub" ] 
+        self.download.gituser = 'eigenteam'
+        self.download.gitrepo = 'eigen-git-mirror'
 
         self.reqfiles = [
             ["include/eigen3/Eigen/src/Core/Matrix.h", "include/eigen3/Eigen/Core",
              "Eigen/src/Core/Matrix.h", "Eigen/Core" ]
         ]
-
+        
+    def compile(self):
+        """ compile Eigen """
+	     	
+        trymakedir( self.buildPath )
+        os.chdir( self.buildPath )
+        
+        self.envcmake["CMAKE_INSTALL_PREFIX"] = self.installPath
+        
+        if( self.rebuild ):
+             tryunlink( "CMakeCache.txt" )
+			
+        # build software
+        if( os.system( self.genCMakeCmd() + " 2>&1 | tee -a " + self.logfile ) != 0 ):
+            self.abort( "failed to configure!!" )
+        
+        if( os.system( ". ../build_env.sh ; make install 2>&1 | tee -a " + self.logfile ) != 0 ):
+            self.abort( "failed to install!!" )
+    
+    def preCheckDeps(self):
+        pass
+        
     def postCheckDeps(self):
         BaseILC.postCheckDeps(self)
