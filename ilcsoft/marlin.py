ffeebe3bf4f8760221965f919f0f8f074242c9f2
Remi Ete <eteremi@flc03.desy.de>
2019-11-12 18:33:28 +0100

    Missing build_env.sh source before compiling Marlin package

--- ilcsoft/marlin.py
+++ ilcsoft/marlin.py
@@ -46,10 +46,10 @@ class Marlin(BaseILC):
         if( os.system( self.genCMakeCmd() + " 2>&1 | tee -a " + self.logfile ) != 0 ):
             self.abort( "failed to configure!!" )
         
-        if( os.system( "make ${MAKEOPTS} 2>&1 | tee -a " + self.logfile ) != 0 ):
+        if( os.system( ". ../build_env.sh ; make ${MAKEOPTS} 2>&1 | tee -a " + self.logfile ) != 0 ):
             self.abort( "failed to compile!!" )
 
-        if( os.system( "make install 2>&1 | tee -a " + self.logfile ) != 0 ):
+        if( os.system( ". ../build_env.sh ; make install 2>&1 | tee -a " + self.logfile ) != 0 ):
             self.abort( "failed to install!!" )
 
         # execute ctests
