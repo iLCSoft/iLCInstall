4dcfcb7899c12d3d6cb041bde124fdf3ee0b04f3
Frank Gaede <frank.gaede@desy.de>
2019-11-21 16:13:29 +0100

    use  MYSQL_DIR if set in environment (HEAD/LATEST)

--- releases/v02-01/release-versions.py
+++ releases/v02-01/release-versions.py
@@ -109,6 +109,13 @@ elif platform.system().lower().find('darwin') >= 0:
 # ----- mysql --------------------------------------------------------
 MySQL_version = "5.0.45"
 MySQL_path = platfDefault
+
+# overwrite with a patch set in the environment
+my_mysql_path = os.getenv("MYSQL_DIR", default=None)
+if( my_mysql_path !=  None ):
+    MySQL_path = my_mysql_path
+
+
 #MySQL_path = ilcPath + "/mysql/" + MySQL_version
 
 ##########################################################################################
