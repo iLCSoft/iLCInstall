50072b84b0fac7e7d6036c29e65d362ddc170d6c
Thomas Madlener <thomas.madlener@desy.de>
2022-01-28 09:44:40 +0100

    Make setup based on LCG_101 and use gcc-10.3

--- releases/HEAD/release-versions-HEAD.py
+++ releases/HEAD/release-versions-HEAD.py
@@ -26,18 +26,20 @@ except KeyError:
 # use a compiler that knows c++17, run
 #
 '''
-# --- gcc from LCG_99
-source /cvmfs/sft.cern.ch/lcg/releases/gcc/10.1.0/x86_64-centos7/setup.sh
+# --- gcc from LCG_101
+source /cvmfs/sft.cern.ch/lcg/releases/gcc/10.3.0/x86_64-centos7/setup.sh
 
-# --- python from LCG_99
-export PATH=/cvmfs/sft.cern.ch/lcg/releases/Python/3.8.6-3199b/x86_64-centos7-gcc10-opt/bin:${PATH}
-export LD_LIBRARY_PATH=/cvmfs/sft.cern.ch/lcg/releases/Python/3.8.6-3199b/x86_64-centos7-gcc10-opt/lib:${LD_LIBRARY_PATH}
+# --- python from LCG_101
+export PATH=/cvmfs/sft.cern.ch/lcg/releases/Python/3.9.6-b0f98/x86_64-centos7-gcc10-opt/bin:${PATH}
+export LD_LIBRARY_PATH=/cvmfs/sft.cern.ch/lcg/releases/Python/3.9.6-b0f98/x86_64-centos7-gcc10-opt/lib:${LD_LIBRARY_PATH}
+export PYTHONPATH=/cvmfs/sft.cern.ch/lcg/views/LCG_101/x86_64-centos7-gcc10-opt/lib/python3.9/site-packages
 
-# --- git from LCG_99
+# --- git from LCG_101
 export PATH=/cvmfs/sft.cern.ch/lcg/releases/git/2.29.2-e475b/x86_64-centos7-gcc10-opt/bin:${PATH}
+export GIT_EXEC_PATH=/cvmfs/sft.cern.ch/lcg/releases/git/2.29.2-e475b/x86_64-centos7-gcc10-opt/libexec/git-core
 
-# --- use a suitable mysql (also LCG_99)
-export MYSQL_DIR=/cvmfs/sft.cern.ch/lcg/releases/mysql/10.4.12-8f05c/x86_64-centos7-gcc10-opt
+# --- use a suitable mysql (also LCG_101)
+export MYSQL_DIR=/cvmfs/sft.cern.ch/lcg/releases/mysql/10.4.20-c0154/x86_64-centos7-gcc10-opt
 '''
 # before starting the installation
 #================================================================================
@@ -107,7 +109,7 @@ elif platform.system().lower().find('darwin') >= 0:
    platfDefault = '/usr/local'
 
 # ----- mysql --------------------------------------------------------
-MySQL_version = "10.4.12"
+MySQL_version = "10.4.20"
 MySQL_path = platfDefault
 
 # overwrite with a patch set in the environment
