29ab963c86689b225d6d67374508530ce73e3f23
Thomas Madlener <thomas.madlener@desy.de>
2024-11-26 12:28:27 +0100

    Pick up Qt5 and XRootD from the underlying LCG stack
    Disable clad for ROOT because it doesn't build

--- scripts/use_gcc131_cvmfs_el9.sh
+++ scripts/use_gcc131_cvmfs_el9.sh
@@ -14,3 +14,15 @@ export GIT_EXEC_PATH=/cvmfs/sft.cern.ch/lcg/releases/git/2.29.2-daa17/x86_64-el9
 
 # --- use a suitable mysql (also LCG_106)
 export MYSQL_DIR=/cvmfs/sft.cern.ch/lcg/releases/mysql/10.5.20-7d082/x86_64-el9-gcc13-opt
+
+# --- pick up Qt5 from LCG release
+export CMAKE_PREFIX_PATH=/cvmfs/sft.cern.ch/lcg/releases/qt5/5.15.9-c981a/x86_64-el9-gcc13-opt:${CMAKE_PREFIX_PATH}
+export LD_LIBRARY_PATH=/cvmfs/sft.cern.ch/lcg/releases/qt5/5.15.9-c981a/x86_64-el9-gcc13-opt/lib:${LD_LIBRARY_PATH}
+export PATH=/cvmfs/sft.cern.ch/lcg/releases/qt5/5.15.9-c981a/x86_64-el9-gcc13-opt/bin:${PATH}
+export QT_PLUGIN_PATH=/cvmfs/sft.cern.ch/lcg/releases/qt5/5.15.9-c981a/x86_64-el9-gcc13-opt/plugins
+export QT_QMAKE_EXECUTABLE=/cvmfs/sft.cern.ch/lcg/releases/qt5/5.15.9-c981a/x86_64-el9-gcc13-opt/bin/qmake
+
+# --- also pick up xrootd from LCG release (as ROOT fails to build without this for whatever reason)
+export PATH=/cvmfs/sft.cern.ch/lcg/releases/xrootd/5.6.9-2f3d3/x86_64-el9-gcc13-opt/bin:${PATH}
+export CMAKE_PREFIX_PATH=/cvmfs/sft.cern.ch/lcg/releases/xrootd/5.6.9-2f3d3/x86_64-el9-gcc13-opt:${CMAKE_PREFIX_PATH}
+export LD_PREFIX_PATH=/cvmfs/sft.cern.ch/lcg/releases/xrootd/5.6.9-2f3d3/x86_64-el9-gcc13-opt/lib64:${CMAKE_PREFIX_PATH}
