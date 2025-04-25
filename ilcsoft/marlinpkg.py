76c7fe53f868c1301ca5841a2931ad0178f996b6
Thomas Madlener <thomas.madlener@desy.de>
2025-04-25 22:16:06 +0200

    Also set install prefix where information is available

--- ilcsoft/marlinpkg.py
+++ ilcsoft/marlinpkg.py
@@ -37,11 +37,11 @@ class MarlinPKG(BaseILC):
         # Marlin packages just provide libraries. 
         # They are not supposed to be found via CMake 
         self.hasCMakeFindSupport = False
-        self.envcmake["CMAKE_INSTALL_PREFIX"] = self.installPath + "/install"
 
     def setMode(self, mode):
         BaseILC.setMode(self, mode)
 
+        self.envcmake["CMAKE_INSTALL_PREFIX"] = self.installPath + "/install"
         self.cmakeconfig = self.installPath + "/install"
 
     def compile(self):
